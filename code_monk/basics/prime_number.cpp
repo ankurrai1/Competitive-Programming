b5c654bc6915ae751ce4b3e5866c71767d09caed
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-01-23 12:10:52 +0530

    added code monk prime number's solution vesson 2

--- code_monk/basics/prime_number.cpp
+++ code_monk/basics/prime_number.cpp
@@ -1,3 +1,4 @@
+// VERSION 1
 #include <iostream>
 
 using namespace std;
@@ -30,3 +31,38 @@ bool is_prime(int num){
     }
     return true;
 }
+
+
+// VERSION 2
+
+// #include <iostream>
+//
+// using namespace std;
+//
+//
+// void print_prime(int);
+// void test_and_print(int);
+//
+// int main() {
+// 	int num;
+// 	cin >> num;
+// 	print_prime(num);
+// 	return 0;
+// }
+//
+// void print_prime(int n){
+//     cout << 2 << " ";
+//     for(int i = 3; i<n; i+=2){
+//         test_and_print(i);
+//     }
+// }
+//
+// void test_and_print(int num){
+//     for(int j=2; j*j<=num; j++){
+//         if(num%j == 0){
+//             return;
+//         }
+//     }
+//     cout << num << " ";
+//     return;
+// }
