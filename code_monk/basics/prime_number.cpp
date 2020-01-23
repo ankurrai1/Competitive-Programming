329cd888e697a9a72fedd84bdfac9e06d02a0953
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-01-23 12:23:14 +0530

    added hacker rank simple array sum function

--- code_monk/basics/prime_number.cpp
+++ code_monk/basics/prime_number.cpp
@@ -1,47 +1,11 @@
 // VERSION 1
-#include <iostream>
-
-using namespace std;
-
-
-void print_prime(int);
-bool is_prime(int);
-
-int main() {
-	int num;
-	cin >> num;
-	print_prime(num);
-	return 0;
-}
-
-void print_prime(int n){
-    for(int i = 0; i<n; i++){
-        if(is_prime(i)){
-            cout << i << " ";
-        }
-    }
-}
-
-bool is_prime(int num){
-    if(num <= 1) return false;
-    for(int j=2; j<num-1; j++){
-        if(num%j == 0){
-            return false;
-        }
-    }
-    return true;
-}
-
-
-// VERSION 2
-
 // #include <iostream>
 //
 // using namespace std;
 //
 //
 // void print_prime(int);
-// void test_and_print(int);
+// bool is_prime(int);
 //
 // int main() {
 // 	int num;
@@ -51,18 +15,54 @@ bool is_prime(int num){
 // }
 //
 // void print_prime(int n){
-//     cout << 2 << " ";
-//     for(int i = 3; i<n; i+=2){
-//         test_and_print(i);
+//     for(int i = 0; i<n; i++){
+//         if(is_prime(i)){
+//             cout << i << " ";
+//         }
 //     }
 // }
 //
-// void test_and_print(int num){
-//     for(int j=2; j*j<=num; j++){
+// bool is_prime(int num){
+//     if(num <= 1) return false;
+//     for(int j=2; j<num-1; j++){
 //         if(num%j == 0){
-//             return;
+//             return false;
 //         }
 //     }
-//     cout << num << " ";
-//     return;
+//     return true;
 // }
+
+
+// VERSION 2
+
+#include <iostream>
+
+using namespace std;
+
+
+void print_prime(int);
+void test_and_print(int);
+
+int main() {
+	int num;
+	cin >> num;
+	print_prime(num);
+	return 0;
+}
+
+void print_prime(int n){
+    cout << 2 << " ";
+    for(int i = 3; i<n; i+=2){
+        test_and_print(i);
+    }
+}
+
+void test_and_print(int num){
+    for(int j=2; j*j<=num; j++){
+        if(num%j == 0){
+            return;
+        }
+    }
+    cout << num << " ";
+    return;
+}
