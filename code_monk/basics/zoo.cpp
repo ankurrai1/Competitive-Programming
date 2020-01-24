9f387fb3b7183abbae2eafa06829df8ac9c4097b
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-01-25 01:31:10 +0530

    added code monk zoo word matching function

---
+++ code_monk/basics/zoo.cpp
@@ -0,0 +1,20 @@
+// link of Question
+// https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/is-zoo-f6f309e7/
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string is;
+    bool flag = true;
+    int z=0,o=0;
+	cin >> is;
+	while(flag){
+	    if((int)is[z]==122) z++;
+	    else flag = false;
+	}
+	o = is.size() - z;
+	if(z*2 == o) cout<<"Yes";
+	else cout<<"No";
+	return 0;
+}
