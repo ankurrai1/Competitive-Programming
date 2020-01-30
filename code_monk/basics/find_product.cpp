b950e39243d18761e8812dca4df3cb4767ce5302
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-01-31 01:41:14 +0530

    added solution for Find Product problem at code monk

---
+++ code_monk/basics/find_product.cpp
@@ -0,0 +1,18 @@
+// Find Product link is as following
+// https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/find-product/description/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int t,n;
+	cin >> t;
+	long prod = 1;
+	for(int i = 0; i < t; i++){
+	    cin >> n;
+	    prod = (prod*n)%(int)(pow(10,9)+7);
+	}
+	cout << prod;
+	return 0;
+}
