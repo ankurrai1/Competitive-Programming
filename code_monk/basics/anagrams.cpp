1172587588751ef9ec91c642eaec6ffc48d6520c
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-02-05 00:11:54 +0530

    added solution for anagrams problem on code monk

--- code_monk/basics/anagrams.cpp
+++ code_monk/basics/anagrams.cpp
@@ -1,2 +1,33 @@
 // problem link is as following
 //https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/anagrams-651/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int get_deletion_c(string,string);
+
+int main() {
+    int t;
+    cin >> t ;
+    for(int i = 0; i < t; i++ ){
+        string a,b;
+        cin >> a;
+        cin >> b;
+        cout << get_deletion_c(a,b)<<"\n";
+    }
+}
+
+int get_deletion_c(string a, string b){
+  int t = 0,as = a.size(),bs = b.size();
+  for(int i = 0; i < as; i++){
+      size_t f = b.find(a[i]);
+      if(f != string::npos){
+          a[i]=' ';
+          b[f]=' ';
+          t += 2;
+      }
+  }
+  return (as+bs-t);
+}
