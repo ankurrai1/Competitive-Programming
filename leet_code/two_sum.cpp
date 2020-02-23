aa1e046e6848d7cdcd4973f4fe8861db6666ed4b
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-02-23 23:34:51 +0530

    added solution for first leet code problem with brute force method

---
+++ leet_code/two_sum.cpp
@@ -0,0 +1,25 @@
+// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+// problem question link is as following
+//https://leetcode.com/problems/two-sum/
+
+// Solution is as following in this only solution function written
+
+
+// high complexity solution is implimented brute force solution
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> v;
+        for(int i = 0; i < nums.size(); i++){
+            for (int j = i+1; j < nums.size(); j++){
+                if(nums[i] + nums[j] == target){
+                     v.push_back(i);
+                     v.push_back(j);
+                     break;
+                }
+            }
+        }
+        return v;
+    }
+};
