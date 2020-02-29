04404484de524449b49c7f0268d590dd568cb9de
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-02-29 23:32:17 +0530

    added code monk solution of Toggle String  problem

--- hacker_rank/birthdayCakeCandles.cpp
+++ hacker_rank/birthdayCakeCandles.cpp
@@ -2,6 +2,7 @@
 //https://www.hackerrank.com/challenges/birthday-cake-candles/problem?h_r=next-challenge&h_v=zen
 //only solution function is as following
 
+
 int birthdayCakeCandles(vector<int> ar) {
     int i, re = 0,max = 0;
     for(i=0; i<ar.size(); i++){
