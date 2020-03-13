5b2e24d2f0a178a0ca7b5e5ccf3ed411fd9182fc
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-03-13 15:53:31 +0530

    added hacker rank solution of days of progremmer

---
+++ hacker_rank/dayOfTheProgremmer.cpp
@@ -0,0 +1,24 @@
+// problem lint is as following
+//https://www.hackerrank.com/challenges/day-of-the-programmer/problem
+// only solution functon is there;
+
+int getDate(int year){
+    int date = 13;
+    if(year <= 1917 & year % 4 == 0){
+        date = 12;
+    }
+    else if (year > 1918){
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)){
+            date = 12;
+        }
+    }
+    return date;
+}
+
+string dayOfProgrammer(int year) {
+    if(year == 1918){
+        return "26.09.1918";
+    }
+    int date = getDate(year);
+    return to_string(date)+ ".09." + to_string(year);
+}
