46e4b85468b7dfa065ecd6d5ce82aaf7192b38fb
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-03-18 18:20:14 +0530

    modified solution for hacker rank bon appetite problem

--- hacker_rank/bon_appétit.cpp
+++ hacker_rank/bon_appétit.cpp
@@ -1,6 +1,9 @@
 // added solution function for problem Bon Appétit at hacker rank
 
+// problem statement is at following like
+//https://www.hackerrank.com/challenges/bon-appetit/problem?h_r=next-challenge&h_v=zen
 
+// Solution function is as follwing
 void bonAppetit(vector<int> bill, int k, int b) {
     int sum = 0;
     for(int i = 0; i < bill.size(); i++){
