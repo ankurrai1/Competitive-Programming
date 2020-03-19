257fca3fea5dfc34240a3cadc7e73679898a5a36
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-03-19 16:39:09 +0530

    modified solution for hacker rank Sock Merchant problem

--- hacker_rank/sock_merchant.cpp
+++ hacker_rank/sock_merchant.cpp
@@ -2,3 +2,22 @@
 
 // Problem statement link is as following
 //https://www.hackerrank.com/challenges/sock-merchant/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+
+// solution function of problem statement is as following
+
+int sockMerchant(int n, vector<int> ar) {
+    std::map<std::string, int> socks;
+    int pairs = 0;
+    for(int i = 0; i < n; i++){
+        string key = to_string(ar[i]);
+        if(socks[key] && socks[key]==1){
+            pairs++;
+            socks[key] = 0;
+        }
+        else {
+            socks[key] = 1;
+        }
+    }
+    return pairs;
+}
