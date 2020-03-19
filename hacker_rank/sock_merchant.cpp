a08ae11e0ac4c641547c55ea71bd9da51f7904c9
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-03-19 16:37:35 +0530

    modified solution for hacker rank Sock Merchant problem

--- hacker_rank/sock_merchant.cpp
+++ hacker_rank/sock_merchant.cpp
@@ -1,1 +1,4 @@
 // solution function for hackerrank Problem Sock Merchant is as following
+
+// Problem statement link is as following
+//https://www.hackerrank.com/challenges/sock-merchant/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
