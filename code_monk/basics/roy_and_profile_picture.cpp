2b738c56a4d5262e66d96d2b0bfa89e2d9d6e3d7
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-03-21 18:31:24 +0530

    added solution for code monk's Roy and Profile Picture problem

---
+++ code_monk/basics/roy_and_profile_picture.cpp
@@ -0,0 +1,6 @@
+// problem statement Roy and Profile Picture is solved here
+
+// codemonk problem Roy and Profile Picture's link is as following
+//https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/roy-and-profile-picture/
+
+// solution
