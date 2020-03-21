5b8dd643cb7bc89d126e8de7073f0ab6cb873f3e
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-03-21 18:31:57 +0530

    added solution for code monk's Roy and Profile Picture problem

--- code_monk/basics/roy_and_profile_picture.cpp
+++ code_monk/basics/roy_and_profile_picture.cpp
@@ -4,3 +4,29 @@
 //https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/roy-and-profile-picture/
 
 // solution
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int size,t;
+	cin >> size;
+	cin >> t;
+	while(t > 0){
+		int a, b;
+		cin >> a >> b;
+		if(a < size || b < size){
+			cout << "UPLOAD ANOTHER";
+		}
+		else if(a == b){
+			cout << "ACCEPTED";
+		}
+		else{
+			cout << "CROP IT";
+		}
+		cout << "\n";
+		t--;
+	}
+	return 0;
+}
