3ec87ba3a280ba723610f5890959cd8f4a78bfb1
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-03-22 22:02:43 +0530

    added solution function of problem Little Elephant and Candies from codechef

--- codeChef/LECandy.cpp
+++ codeChef/LECandy.cpp
@@ -2,3 +2,24 @@
 
 //link of problem is following
 //https://www.codechef.com/problems/LECANDY
+
+// solution for problem is as following in c++
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t,n,d;
+    cin >> t;
+    for(int i = 0; i < t; i++){
+        cin >> n >> d;
+        for(int j = 0; j < n; j++){
+            int m;
+            cin >> m;
+            d -= m;
+        }
+        if(d>=0) cout << "Yes\n";
+        else cout << "No\n";
+    }
+	return 0;
+}
