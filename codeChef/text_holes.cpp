2e9c18f822a6d4d2eabe8a4b63c085e36bbb3062
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-03-23 23:09:55 +0530

    added solution file for codechef Holes in the text problem

--- codeChef/text_holes.cpp
+++ codeChef/text_holes.cpp
@@ -2,3 +2,23 @@
 
 // problem statement link is as following
 // https://www.codechef.com/problems/HOLES
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    while(n--){
+        string s;
+        int holes = 0;
+        cin >> s;
+        for(int i = 0; i < s.length(); i++){
+            if(s[i] == 'A'|| s[i] == 'D'|| s[i]=='O'||s[i] == 'P'|| s[i]=='Q'||
+            s[i]=='R') holes++;
+            else if(s[i] == 'B') holes += 2;
+        }
+        cout << holes <<"\n";
+    }
+	return 0;
+}
