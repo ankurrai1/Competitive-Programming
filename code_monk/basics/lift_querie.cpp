9b0c486038777ed09f919129fc2844e71a471846
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-03-25 00:48:39 +0530

    added solution file for code monk problem  Lift queries

--- code_monk/basics/lift_querie.cpp
+++ code_monk/basics/lift_querie.cpp
@@ -1,1 +1,27 @@
 // problem link is as following
+//https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/lift-queries/
+
+
+
+//solution function is as following
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int t,al = 0, bl = 7;
+	cin >> t;
+	for(int i = 0; i < t; i++){
+		int f;
+		cin >> f;
+		if(abs(al - f) <= abs(bl - f)){
+			cout <<"A\n";
+			al = f;
+		}
+		else{
+			cout << "B\n";
+			bl = f;
+		}
+	}
+	return 0;
+}
