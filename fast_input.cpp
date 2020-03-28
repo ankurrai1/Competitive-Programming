c401836a0b0fc69c5e2fec4b4d9f80c91a89ed5b
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-03-28 23:37:55 +0530

    git fast input out put file to a pp

--- fast_input.cpp
+++ fast_input.cpp
@@ -7,9 +7,9 @@ int main()
     // added the two lines below
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
+    // code goes here as regular
     cin >> n >> k;
     cout << (n+k) << "\n";
-    
+    // In above line to the place of endln we should use "\n" which is way faster
     return 0;
 }
