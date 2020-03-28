4be77e53620534db734e7c781b83016fb7897810
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-03-28 23:35:55 +0530

    git fast input out put file to a pp

--- fast_input.cpp
+++ fast_input.cpp
@@ -1,1 +1,15 @@
 // this is basic file to use as templet for most of the competitive coding problems
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // added the two lines below
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n >> k;
+    cout << (n+k) << "\n";
+    
+    return 0;
+}
