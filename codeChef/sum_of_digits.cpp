cbd22584701964d5bd16863e7346cc553d2446d7
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-03-29 17:02:08 +0530

    added solution function for file

--- codeChef/sum_of_digits.cpp
+++ codeChef/sum_of_digits.cpp
@@ -1,6 +1,34 @@
 // it is the solution file for problem of sum of digits
 
 // problem link is as following
-
+//https://www.codechef.com/problems/FLOW006
 
 // solution function implimented here
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int get_sum(int);
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int c;
+	cin >> c;
+	for(int i = 0; i < c; i++){
+	    int num;
+	    cin >> num;
+	    int sum = get_sum(num);
+	    cout << sum << "\n";
+	}
+	return 0;
+}
+
+int get_sum(int n){
+    int sum = 0;
+    while(n > 0){
+        sum += n%10;
+        n = n / 10;
+    }
+    return sum;
+}
