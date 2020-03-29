dc6ccf8abb6e7ca30bfa4dd3fb9904964918f1fe
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-03-29 17:56:01 +0530

    added solution for codechef first and last digit problem

--- codeChef/first_and_last.cpp
+++ codeChef/first_and_last.cpp
@@ -4,3 +4,25 @@
 //
 
 // solution function is as following
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n,i;
+    cin >> n;
+    for(i = 0; i < n; i++){
+        int num,l,f;
+        cin >> num;
+        l = num % 10;
+        while(num >= 10){
+            num = num / 10;
+        }
+    cout << (num + l ) << "\n";
+    }
+    return 0;
+}
