75066f4aabbda4f445889d69f06495d5d043c228
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-04-04 19:43:00 +0530

    added solution function for Grading Students problem of hacker rank

--- hacker_rank/grading_students.cpp
+++ hacker_rank/grading_students.cpp
@@ -1,3 +1,15 @@
 // this is solution file for problem of question Grading Students
 
 // problem statement link
+
+vector<int> gradingStudents(vector<int> grades) {
+    for(int i  = 0; i < grades.size(); i++){
+        int val = grades[i];
+        if(val >= 38){
+            if(5 - (val % 5)< 3){
+                grades[i] = val + (5 - (val % 5));
+            }
+        }
+    }
+    return grades;
+}
