a8a0add4eeabd4508a28a2508cce9cff4484cc4f
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-04-07 01:37:15 +0530

    added geeksforgeeks folder and rotate array problem's solution

--- geeksforgeeks/rotate_array.cpp
+++ geeksforgeeks/rotate_array.cpp
@@ -2,3 +2,24 @@
 
 // problem link is as following
 //https://practice.geeksforgeeks.org/problems/rotate-array-by-n-elements/0
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for(int i = 0; i<n; i++){
+        int l,d,p = 0;
+        cin >>l>> d;
+        int ar[l];
+        for(int j = 0; j < l; j++){
+            cin >> ar[(j + l -d) % l];
+        }
+        for(int s = 0; s < l; s++){
+            cout << ar[s] <<" ";
+        }
+        cout << "\n";
+    }
+	return 0;
+}
