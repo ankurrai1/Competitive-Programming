5a6248b65c13d6dd2566efd979f32a0a3d743c96
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-04-07 03:29:06 +0530

    added geeksforgeeks folder and Leaders in an array  problem's  solution

--- geeksforgeeks/leaders_in_array.cpp
+++ geeksforgeeks/leaders_in_array.cpp
@@ -1,2 +1,37 @@
 // Leaders in an array solution file
 // problem link is as following
+// https://practice.geeksforgeeks.org/problems/leaders-in-an-array/0/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int t;
+  cin >> t;
+  while(t--){
+    int n,i;
+    cin >> n;
+    int ar[n];
+
+    for(i=0;i<n;i++){
+      cin >> ar[i];
+    }
+    int max = ar[n-1];
+    for(i=n-2;i>=0;i--){
+      if(max <= ar[i]){
+          max = ar[i];
+      }
+      else ar[i] = -1;
+    }
+
+    for(i=0;i<n;i++){
+      if(ar[i] != -1){
+        cout << ar[i] << " ";
+      }
+    }
+    cout << "\n";
+  }
+  return 0;
+}
