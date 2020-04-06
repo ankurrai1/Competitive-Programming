f28f320447fdc77bea65bebdeba8f76b3f379bfb
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-04-07 05:04:25 +0530

    added geeksforgeeks Kth smallest element problem's solution

--- geeksforgeeks/Kth_smallest_element.cpp
+++ geeksforgeeks/Kth_smallest_element.cpp
@@ -1,2 +1,38 @@
 // Kth smallest element solution file
 // problem link is as following
+// https://practice.geeksforgeeks.org/problems/kth-smallest-element/0
+
+
+#include <bits/stdc++.h>
+#include <queue>
+
+using namespace std;
+
+struct comparator {
+ bool operator()(int i, int j) {
+ return i > j;
+ }
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--){
+        int n,i,l;
+        cin >> n;
+         priority_queue<int, std::vector<int>, comparator> minHeap;
+        for(i = 0; i < n; i++){
+            cin >> l;
+            minHeap.push(l);
+        }
+        cin >> l;
+        for(i = 1; i < l; i++){
+            minHeap.pop();
+        }
+        cout <<(minHeap.top())<< "\n";
+    }
+    return 0;
+}
