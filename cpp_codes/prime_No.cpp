9321c5b817eed5b24f4fe8bf7d4ce9ee1fd43fa9
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-04-10 03:11:54 +0530

    added fast prime finding to cpp learning

--- cpp_codes/prime_No.cpp
+++ cpp_codes/prime_No.cpp
@@ -7,17 +7,18 @@ using namespace std;
 
 int main(){
   int i,n;
+  bool isPrime = true;
   cin >> n;
-  if(n%2 == 0) cout << "not prime No.";
+  if(n%2 == 0) isPrime = false;
   else{
     for(i = 3; i < n/2; i+=2){
       if(n%i == 0){
-        cout << "not prime No.";
+        isPrime = false;
         break;
       }
     }
-    cout << "No is a Prime ."
   }
-  for(i = ; i < n/2; i+=2)
+  if(isPrime) cout << "No is a Prime .";
+  else cout << "not prime No.";
   return 0;
 }
