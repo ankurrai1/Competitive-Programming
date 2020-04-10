241e14fc085a46d536dfb04b3ca9dadfdbb20a78
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-04-11 03:07:26 +0530

    added solution file for hackerrank problem of apple and oranges

---
+++ hacker_rank/apple_and_oranges.cpp
@@ -0,0 +1,7 @@
+// here hackerrank problem Apple and Oranges are solved
+
+//problem link is as following
+// https://www.hackerrank.com/challenges/apple-and-orange/problem
+
+
+// solution function is as following
