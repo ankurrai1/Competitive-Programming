5faeaac21ed1ada8894940784b4edccb256de832
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-04-11 01:09:21 +0530

    added pascale Tringle printing function in repo

---
+++ cpp_codes/patterns/pascal_tringle.cpp
@@ -0,0 +1,43 @@
+// 5
+//      1
+//     1 1
+//    1 2 1
+//   1 3 3 1
+//  1 4 6 4 1
+
+// above is pascal Tringle it is printed by following code
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void pascalTringle(int n){
+  int ar[n][n];
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j <= i; j++){
+      if(j == 0 || j == i) ar[i][j] = 1;
+       else{
+         ar[i][j] = ar[i-1][j-1] + ar[i-1][j];
+       }
+    }
+  }
+
+  for(int i = 0; i < n; i++){
+    for(int k = 0; k < n-i; k++){
+        cout << " ";
+    }
+    for(int j = 0; j <= i; j++){
+      cout << ar[i][j] << " ";
+    }
+    cout << "\n";
+  }
+
+}
+
+int main(){
+  int n;
+  cin >> n;
+  pascalTringle(n);
+  return 0;
+}
