6fbae62a8f6e68e8ba1047b127b46fa0d185d108
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-04-11 03:33:25 +0530

    added solution file for hackerrank problem of Jumping on the Clouds

---
+++ hacker_rank/jumping_on_clouds.cpp
@@ -0,0 +1,7 @@
+// problem Jumping on the Clouds of hackerrank is soleved here
+
+// problem statement link is as following
+
+
+// solution function is as following
+//https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
