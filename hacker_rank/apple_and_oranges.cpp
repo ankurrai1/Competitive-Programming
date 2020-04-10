27cc807a7749f8dab10695da505b3ab1df30abe8
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-04-11 03:07:51 +0530

    added solution file for hackerrank problem of apple and oranges

--- hacker_rank/apple_and_oranges.cpp
+++ hacker_rank/apple_and_oranges.cpp
@@ -5,3 +5,16 @@
 
 
 // solution function is as following
+
+void countApplesAndOranges(int s, int t, int a, int b, vector<int> apples, vector<int> oranges) {
+    int i, appleCount = 0, orangeCount = 0, range;
+    for(i = 0; i < apples.size(); i++){
+        range = apples[i] + a;
+        if(range >= s && range <= t) appleCount++;
+    }
+    for(i = 0; i < oranges.size(); i++){
+        range = oranges[i] + b;
+        if(range >= s && range <= t) orangeCount++;
+    }
+    cout << appleCount << "\n" << orangeCount;
+}
