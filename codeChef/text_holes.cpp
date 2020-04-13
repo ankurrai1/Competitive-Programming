7047ac62d52bf77f8162b2e29cfbbd86a5dc6039
Ankur rai <rai.ankur0007@gmail.com>
2020-04-13 23:34:19 +0530

    Update text_holes.cpp

--- codeChef/text_holes.cpp
+++ codeChef/text_holes.cpp
@@ -14,7 +14,7 @@ int main() {
         int holes = 0;
         cin >> s;
         for(int i = 0; i < s.length(); i++){
-            if(s[i] == 'A'|| s[i] == 'D'|| s[i]=='O'||s[i] == 'P'|| s[i]=='Q'||
+            if(s[i] == 'A'|| s[i] == 'D'|| s[i]=='O'||s[i] == 'P'|| s[i]=='Q'||       // these are text has only one hole
             s[i]=='R') holes++;
             else if(s[i] == 'B') holes += 2;
         }
