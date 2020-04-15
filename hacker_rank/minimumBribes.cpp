b4ef1d16f8207c66541fda58e1bc933dc3748ed6
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-04-16 03:07:53 +0530

    added minimum Bribes problem solution for hacker rank

--- hacker_rank/minimumBribes.cpp
+++ hacker_rank/minimumBribes.cpp
@@ -4,3 +4,19 @@
 
 
 // Completed the minimumBribes function is below.
+
+void minimumBribes(vector<int> q) {
+    int bribes = 0;
+    for(int i = q.size() - 1; i >= 0; i--){
+        int offset = q[i] -i -1;
+        if(offset > 2) {
+            cout << "Too chaotic\n";
+            return;
+        }
+        for(int j = max(0,q[i]-2); j < i; j++){
+            if(q[j] > q[i]) bribes++;
+        }
+    }
+    cout << bribes;
+    cout << "\n";
+}
