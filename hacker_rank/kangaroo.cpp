a40357fff02a43ac262dbd5e8b9fecd891aa3ef3
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-04-17 02:08:41 +0530

    added kangaroo problem solution for hacker rank

--- hacker_rank/kangaroo.cpp
+++ hacker_rank/kangaroo.cpp
@@ -5,3 +5,9 @@
 //https://www.hackerrank.com/challenges/kangaroo/problem\
 
 // solution function is as following
+
+string kangaroo(int x1, int v1, int x2, int v2) {
+    if(v2 >= v1) return "NO";
+    if(abs(x1 - x2) % (v2 - v1) == 0) return "YES";
+    return "NO";
+}
