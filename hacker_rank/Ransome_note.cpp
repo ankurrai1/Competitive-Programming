7d2252b54044f7c42e4c5712467583dc62a96898
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-04-20 03:08:17 +0530

    added solution  function for hacker rank proble ransom note

--- hacker_rank/Ransome_note.cpp
+++ hacker_rank/Ransome_note.cpp
@@ -2,3 +2,20 @@
 // https://www.hackerrank.com/challenges/ctci-ransom-note/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps
 
 // here only solution function is implimented;
+
+// Complete the checkMagazine function below.
+void checkMagazine(vector<string> magazine, vector<string> note) {
+    unordered_map<string, int> data;
+    for (auto e : magazine){
+        if(data[e]) data[e]++;
+        else data[e] = 1;
+    }
+    for(auto e: note){
+        if(data[e] && data[e] > 0) data[e]--;
+        else{
+            cout << "No"<<"\n";
+            return;
+        }
+    }
+    cout << "Yes"<< "\n";
+}
