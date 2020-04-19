3354dd548baeb8338c934204f06c450ec40da403
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-04-20 04:23:29 +0530

    added solution  function for hacker rank tow string solution function

--- hacker_rank/two_string.cpp
+++ hacker_rank/two_string.cpp
@@ -2,3 +2,22 @@
 
 // problem statement link is as following
 // https://www.hackerrank.com/challenges/two-strings/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps&h_r=next-challenge&h_v=zen
+
+
+// solution function is implimented as following
+
+string twoStrings(string s1, string s2) {
+    unordered_map<char, int> data;
+    for(int i = 0; i < s1.size(); i++){
+        char str = s1[i];
+        if(data[str]) data[str]++;
+        else{
+            data[str] = 1;
+        }
+    }
+    for(int i = 0; i < s2.size(); i++){
+        char str = s2[i];
+        if(data[str]) return "YES";
+    }
+    return "NO";
+}
