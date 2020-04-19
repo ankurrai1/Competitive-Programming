cb7040a8e464ea95b7d06b76848c0faa7b7f37a3
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-04-20 04:22:49 +0530

    added solution  function for hacker rank tow string file

---
+++ hacker_rank/two_string.cpp
@@ -0,0 +1,4 @@
+// the problem is to find whether the first string has some sub string of second string.
+
+// problem statement link is as following
+// https://www.hackerrank.com/challenges/two-strings/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps&h_r=next-challenge&h_v=zen
