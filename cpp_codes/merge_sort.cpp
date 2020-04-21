4c0aa9b718037ac0772cf6a3a32e16e7c7694dc8
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-04-22 00:03:17 +0530

    added merge sort implimentation merge sort recurcive function

--- cpp_codes/merge_sort.cpp
+++ cpp_codes/merge_sort.cpp
@@ -1,13 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-
-
+// =============================================================================
+// recursive merge sort function implementation here and array is passed by reference.
+void merge_sort(int &arr[], int l, int r){
+  if(l != r){
+    int mid = (l + r)/2;
+    merge_sort(arr,l,mid);
+    merge_sort(arr,mid+1,r);
+    merge(arr,l,mid,mid+1,r);
+  }
+}
 
 // =================================================================
 // runner function is as following.
-
 int main()
 {
     ios_base::sync_with_stdio(false);
