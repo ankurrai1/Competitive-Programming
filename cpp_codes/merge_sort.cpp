5403793f14f33201824dae8b49a7549974cc7c9e
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-04-22 03:56:32 +0530

    added merge sort implimentation merge sort recurcive function

--- cpp_codes/merge_sort.cpp
+++ cpp_codes/merge_sort.cpp
@@ -1,16 +1,58 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+void merge(int *Arr, int start, int mid, int end) {
+	// create a temp array
+	int temp[end - start + 1];
+
+	// crawlers for both intervals and for temp
+	int i = start, j = mid+1, k = 0;
+
+	// traverse both arrays and in each iteration add smaller of both elements in temp
+	while(i <= mid && j <= end) {
+		if(Arr[i] <= Arr[j]) {
+			temp[k] = Arr[i];
+			k += 1; i += 1;
+		}
+		else {
+			temp[k] = Arr[j];
+			k += 1; j += 1;
+		}
+	}
+
+	// add elements left in the first interval
+	while(i <= mid) {
+		temp[k] = Arr[i];
+		k += 1; i += 1;
+	}
+
+	// add elements left in the second interval
+	while(j <= end) {
+		temp[k] = Arr[j];
+		k += 1; j += 1;
+	}
+
+	// copy temp to original interval
+	for(i = start; i <= end; i += 1) {
+		Arr[i] = temp[i - start];
+	}
+};
+
+
+
+
 // =============================================================================
 // recursive merge sort function implementation here and array is passed by reference.
-void merge_sort(int &arr[], int l, int r){
-  if(l != r){
-    int mid = (l + r)/2;
-    merge_sort(arr,l,mid);
-    merge_sort(arr,mid+1,r);
-    merge(arr,l,mid,mid+1,r);
-  }
-}
+void mergeSort(int *Arr, int start, int end) {
+
+	if(start < end) {
+		int mid = (start + end) / 2;
+		mergeSort(Arr, start, mid);
+		mergeSort(Arr, mid+1, end);
+		merge(Arr, start, mid, end);
+	}
+};
 
 // =================================================================
 // runner function is as following.
@@ -21,13 +63,14 @@ int main()
     int n;
     cin >> n;
     int arr[n];
-    for(i = 0; i < n; i++){
-      cin >> arr[i];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
     }
-    merge_sort(arr,0,n-1);
 
-    for(i = 0; i < n; i++){
-      cout << arr[i];
+    mergeSort(arr,0,n-1);
+
+    for(int i = 0; i < n; i++){
+      cout << arr[i] << " ";
     }
     return 0;
 }
