845aa9882513cc6c1a8b0c164a866a933b479d46
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-04-25 00:11:30 +0530

    added solution file for smallest next palindrom problem of spoj

--- spoj/next_smallest_palindrome.cpp
+++ spoj/next_smallest_palindrome.cpp
@@ -1,11 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+string get_next_palindrome(string input){
+  int l = input.length();
+  string npalind = "";
+  cout << l << " I am length of no";
+  return npalind;
+}
+
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int n;
     cin >> n;
+    while (n--){
+      string no,next_palindrome;
+      cin >> no;
+      next_palindrome = get_next_palindrome(no);
+      cout << next_palindrome << "\n";
+    }
     return 0;
 }
