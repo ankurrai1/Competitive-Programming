bbcbae37c52b33614fdc5e02779976c38434af37
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-04-25 00:35:47 +0530

    added git ignore file to reopo

--- spoj/next_smallest_palindrome.cpp
+++ spoj/next_smallest_palindrome.cpp
@@ -2,24 +2,24 @@
 using namespace std;
 
 
-string get_next_palindrome(string input){
-  int l = input.length();
-  string npalind = "";
-  cout << l << " I am length of no";
-  return npalind;
-}
+// string get_next_palindrome(string input){
+//   int l = input.length();
+//   string npalind = "";
+//   cout << l << " I am length of no";
+//   return npalind;
+// }
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int n;
-    cin >> n;
-    while (n--){
-      string no,next_palindrome;
-      cin >> no;
-      next_palindrome = get_next_palindrome(no);
-      cout << next_palindrome << "\n";
-    }
+    // ios_base::sync_with_stdio(false);
+    // cin.tie(NULL);
+    // int n;
+    // cin >> n;
+    // while (n--){
+    //   string no,next_palindrome;
+    //   cin >> no;
+    //   // next_palindrome = get_next_palindrome(no);
+    //   cout << next_palindrome << "\n";
+    // }
     return 0;
 }
