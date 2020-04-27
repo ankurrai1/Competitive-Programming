accaffc934abccc4f6df2765f27cc9563109bef0
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-04-27 19:16:31 +0530

    added day1 of 30 day challange code count equal set of 0 and 1

---
+++ 30daysOfcode./count01.cpp
@@ -0,0 +1,25 @@
+// #Day1
+//Given an array of 0's and 1's your task is to complete the function maxLen
+//which returns size of the largest sub array with equal number of 0's and 1's.
+
+// solution function is as following
+
+int maxLen(int arr[], int n){
+    int res = 0, sum = 0, i;
+    unordered_map <int, int> dataSet;
+    for(i = 0; i < n; i++){
+        if(arr[i] == 0)
+        arr[i] = -1;
+    }
+    dataSet[0] = -1;
+    for(int i = 0; i < n; i++){
+        sum = sum + arr[i];
+        if(dataSet[sum]){
+            res = max(res, i - dataSet[sum]);
+        }
+        else
+        dataSet[sum] = i;
+
+    }
+    return res;
+}
