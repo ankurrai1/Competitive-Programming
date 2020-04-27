8f2ec95652f73b857587d2454cb79eb722e36bf8
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-04-27 11:30:13 +0530

    added day1 of 30 day challange code replace all 0 with 5

---
+++ 30daysOfcode./replace_with_5.cpp
@@ -0,0 +1,16 @@
+// #Day1
+// solution function is as following
+// problem statement
+//You are given an integer N. You need to convert all zeroes of N to 5.
+
+int convertFive(int n) {
+    int i = 0, res = 0, digit;
+    while(n > 0){
+        digit = n % 10;
+        n = n / 10;
+        if(digit == 0) digit = 5;
+        res += digit*(pow(10,i));
+        i++;
+    }
+    return res;
+}
