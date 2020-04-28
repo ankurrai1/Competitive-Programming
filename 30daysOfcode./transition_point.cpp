033d8ed2e267272596d0d79c9629cdaddd196c88
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-04-28 20:30:43 +0530

    added day1 of 30 day challange code Find Transition Point

---
+++ 30daysOfcode./transition_point.cpp
@@ -0,0 +1,6 @@
+// #Day2
+// problem statement si as following
+//You are given a sorted array containing only numbers 0 and 1. Find the transition point efficiently. Transition point is a point where "0" ends and "1" begins.
+// Note that, if there is no "1" exists, print -1.
+
+// complexity will be O(log n)
