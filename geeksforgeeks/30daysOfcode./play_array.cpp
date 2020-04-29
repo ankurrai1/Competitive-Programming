f7983d4334e4cef89f63cdc088b7f2a257ecdb75
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-04-30 04:49:49 +0530

    added day2 of 30 day challange solution for Play with an array problem

--- geeksforgeeks/30daysOfcode./play_array.cpp
+++ geeksforgeeks/30daysOfcode./play_array.cpp
@@ -1,2 +1,9 @@
 // #Day2 30 april 2020
-// problem statement si as following
+// problem statement is as following
+
+//  Given an unsorted array arr of size N, rearrange the elements of array such that
+//  number at the odd index is greater than the number at the previous even index.
+//  The task is to complete the function formatArray() which will return formatted array.
+
+
+// solution function implementation is here:
