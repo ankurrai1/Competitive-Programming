b49d178a0ce75eb333a28f35506c454f31627646
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-04-30 04:30:33 +0530

    added day2 of 30 day challange solution for first negative in every k window

--- geeksforgeeks/30daysOfcode./first_negative.cpp
+++ geeksforgeeks/30daysOfcode./first_negative.cpp
@@ -1,2 +1,4 @@
 // #Day2 30 april 2020
 // problem statement si as following
+//Given an array and a positive integer k, find the first negative integer for each and every,
+// window(contiguous subarray) of size k.
