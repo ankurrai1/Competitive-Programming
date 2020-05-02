7e06fd4f602d93b2118fdc15a07e53b7d9126034
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-02 23:22:56 +0530

    added solution for day 3 problem of geeks for geeks days named as recursive sequence.

--- geeksforgeeks/30daysOfcode./recursive_sequence.cpp
+++ geeksforgeeks/30daysOfcode./recursive_sequence.cpp
@@ -4,3 +4,36 @@
 // Given an integer n the task is to print the F(n)th term.
 
 // solution function is as following
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void fill_with_res(unsigned long long int* a, int n){
+    unsigned long long int num;
+    a[0] = 0;
+    int mul = 1;
+
+    for(int i = 1; i < n; i++){
+        num = 1;
+        for(int j = 0; j < i; j++){
+            num = num*mul;
+            mul++;
+        }
+        a[i] = a[i-1] + num;
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    unsigned long long int res[11];
+    fill_with_res(res,11);
+    int t, n, k = 1;
+    cin >> t;
+    while(t--){
+       cin >> n;
+       cout << res[n]<< "\n";
+    }
+    return 0;
+}
