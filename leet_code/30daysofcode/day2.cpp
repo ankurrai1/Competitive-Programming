fa391ba6b746dc03f832322801ee49b12077faf1
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-02 23:07:28 +0530

    added solution for day 2 problem of leetcode 30 days challange may

---
+++ leet_code/30daysofcode/day2.cpp
@@ -0,0 +1,17 @@
+//  problem statement is as following
+//  You're given strings J representing the types of stones that are jewels, and S representing the stones you have.
+//  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+
+// solution function
+
+class Solution {
+public:
+    int numJewelsInStones(string J, string S) {
+        map<char, int> jewels;
+        for(char e: J) jewels[e]++;
+        int count  = 0;
+        for(char e : S)
+            if(jewels[e]) count++;
+        return count;
+    }
+};
