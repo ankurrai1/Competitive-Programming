dbf89d8df8509de1f16988ecac146c7bd1b7278e
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-02 23:22:11 +0530

    added solution for day 3 problem of geeks for geeks days named as recursive sequence.

---
+++ geeksforgeeks/30daysOfcode./recursive_sequence.cpp
@@ -0,0 +1,6 @@
+// #Day3
+// problem statement is as following
+// A function f is defined as follows F(n)= (1) +(2*3) + (4*5*6) ... n.
+// Given an integer n the task is to print the F(n)th term.
+
+// solution function is as following
