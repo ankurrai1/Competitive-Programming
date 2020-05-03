e3a810ad73c575649ac148c5cc0e2b089ec992e0
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-03 20:59:29 +0530

    added solution for day 3 problem of geeks for geeks 30 days challange may

--- geeksforgeeks/30daysOfcode./inverse_permutation.cpp
+++ geeksforgeeks/30daysOfcode./inverse_permutation.cpp
@@ -4,3 +4,27 @@
 
 
 // solution function implementation here:
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t, n, k = 1;
+    cin >> t;
+    while(t--){
+       cin >> n;
+       int arr[n];
+       int res[n];
+       for(int i = 0; i < n; i++) cin >> arr[i];
+       for(int i = 0; i < n; i++){
+           res[arr[i]-1] = i+1;
+       }
+       for(int i = 0; i < n; i++) cout << res[i] << " ";
+       cout << "\n";
+    }
+    return 0;
+}
