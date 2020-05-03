3043b37afa253716855e5be86f0c490aaadf71db
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-03 20:59:11 +0530

    added solution for day 3 problem of geeks for geeks 30 days challange may

---
+++ geeksforgeeks/30daysOfcode./inverse_permutation.cpp
@@ -0,0 +1,6 @@
+// problem statement is as following
+// Suppose we found element 4 at position 3 in an array, then in reverse permutation,
+//  we insert 3 (position of element 4 in the array) in position 4 (element value)
+
+
+// solution function implementation here:
