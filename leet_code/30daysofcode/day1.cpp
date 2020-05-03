fb3134cbd082ddfb7860e5588f6dff494bf3cd19
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-03 08:38:05 +0530

    added solution for day 1 problem of leetcode 30 days challange may

--- leet_code/30daysofcode/day1.cpp
+++ leet_code/30daysofcode/day1.cpp
@@ -8,3 +8,26 @@ which causes all the following ones to be bad.
 You are given an API bool isBadVersion(version) which will return whether version is bad.
 Implement a function to find the first bad version. You should minimize the number of calls to the API.
 */
+
+
+// solution function is as following:
+
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int findfirst(int s, int e){
+        if(s < e){
+            long int mid = s + (e-s)/2;
+            bool isMidBad = isBadVersion(mid);
+            if(isMidBad && !isBadVersion(mid-1)) return mid;
+            else if(!isMidBad)return findfirst(mid+1, e);
+            else return findfirst(s,mid -1);
+        }
+        return s;
+    }
+    int firstBadVersion(int n) {
+        return findfirst(1,n);
+    }
+};
