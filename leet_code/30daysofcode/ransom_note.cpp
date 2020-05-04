6a6f73768c9db6a37e4d1b8a115b966bb871c391
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-04 12:10:02 +0530

    added solution for day 3 problem of leet code  30 days challange may

--- leet_code/30daysofcode/ransom_note.cpp
+++ leet_code/30daysofcode/ransom_note.cpp
@@ -3,3 +3,20 @@
 // Given an arbitrary ransom note string and another string containing letters from all the magazines,
 //  write a function that will return true if
 //  the ransom note can be constructed from the magazines ; otherwise, it will return false.
+
+
+// solution function is as following
+
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char, int> ransomData;
+
+        for(char e : magazine) ransomData[e]++;
+        for(char e: ransomNote){
+            if(ransomData[e]&& ransomData[e] > 0) ransomData[e]--;
+            else return false;
+        }
+        return true;
+    }
+};
