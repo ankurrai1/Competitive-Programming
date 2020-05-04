726af2267bab9e34478db9288439ba66dd474b0b
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-04 17:10:04 +0530

    added solution for day 4 problem of geeks for geeks 30 days challange may

---
+++ geeksforgeeks/30daysOfcode./numberpad.cpp
@@ -0,0 +1,5 @@
+// #Day4
+// problem statement is as following :
+// Given a sentence in the form of a string in uppercase, convert it into its equivalent mobile numeric keypad sequence.
+// Like
+// A = 2 B=22 C=222; D=3 ...................... and so on.
