991730400eae6cfd8c92ded664c7b5eb95738dac
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-04 13:31:54 +0530

    added solution for day 4 problem of geeks for geeks 30 days challange may

--- geeksforgeeks/30daysOfcode./divisibal8.cpp
+++ geeksforgeeks/30daysOfcode./divisibal8.cpp
@@ -1,3 +1,28 @@
 // #Day4
 // problem statement
 //Given a number n, check if it is divisible by 8.
+
+// solution function is implimented as following
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--){
+        int ans = -1;
+        string num;
+        cin >> num;
+        int sub_part,s = num.size();
+        if(s <= 3) sub_part = stoi(num);
+        else sub_part = stoi(num.substr(s-3, 3));
+        if(sub_part % 8 == 0) ans = 1;
+        cout << ans <<"\n";
+    }
+    return 0;
+}
