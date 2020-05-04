5f0ad475a9770197e6ab7637e3a57f9fac1378be
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-04 12:09:24 +0530

    added solution for day 3 problem of leet code  30 days challange may

---
+++ leet_code/30daysofcode/ransom_note.cpp
@@ -0,0 +1,5 @@
+// Problem statement is as following
+
+// Given an arbitrary ransom note string and another string containing letters from all the magazines,
+//  write a function that will return true if
+//  the ransom note can be constructed from the magazines ; otherwise, it will return false.
