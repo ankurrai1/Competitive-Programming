eb5b010b8d5e5ce06a19ffe2619ccc13ec9ecf36
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-05 14:10:42 +0530

    added solution for day 4 problem of leet code may number compliment

--- leet_code/30daysofcode/number_complement.cpp
+++ leet_code/30daysofcode/number_complement.cpp
@@ -2,3 +2,12 @@
 // problem statement is as following:
 // Given a positive integer, output its complement number.
 // The complement strategy is to flip the bits of its binary representation.
+
+// Solution function is as following:
+
+int findComplement(int num) {
+    int x=1;
+    while(pow(2,x)-1<num)
+        x++;
+    return pow(2,x)-1-num;
+}
