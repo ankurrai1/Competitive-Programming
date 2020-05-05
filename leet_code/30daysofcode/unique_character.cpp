93e37ae16ad60ffd2d7bfcc768c608c9ae8c4167
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-05 16:13:52 +0530

    added solution for day 5 problem of leet code may number compliment

---
+++ leet_code/30daysofcode/unique_character.cpp
@@ -0,0 +1,6 @@
+ // day5
+ // problem statement is as:
+ // Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+
+
+// solution function is implemented here.
