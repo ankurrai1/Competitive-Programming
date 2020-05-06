6caa4b54f8f376b2cbce8f4267b77d4c1ac34651
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-06 20:33:17 +0530

    added solution hacker rank problem braking record

---
+++ hacker_rank/braking_record.cpp
@@ -0,0 +1,5 @@
+// problem statement is as following :
+// Given a vector  scores for a season,
+// find and print the number of times she breaks her records for most and least points scored during the season.
+
+// solution function is implemented here:
