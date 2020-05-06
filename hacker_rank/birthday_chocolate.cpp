f8f11424db023205aca6236d3be6a98bdb592788
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-07 00:14:11 +0530

    added solution hacker rank problem birthday chocolate

--- hacker_rank/birthday_chocolate.cpp
+++ hacker_rank/birthday_chocolate.cpp
@@ -3,3 +3,15 @@
 
 
 // solution function implementation is as following :
+
+int birthday(vector<int> s, int d, int m) {
+    int c_sum = 0, res = 0, t = 0;
+    for(int i = 0; i < m; i++) c_sum += s[i];
+    if(c_sum == d) res++;
+    for(int i = m; i < s.size(); i++,t++){
+        c_sum -= s[t];
+        c_sum += s[i];
+        if(c_sum == d) res++;
+    }
+    return res;
+}
