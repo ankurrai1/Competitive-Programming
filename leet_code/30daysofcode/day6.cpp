18121f04db0bac336903a6146da5e2e95ea23687
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-07 10:25:51 +0530

    added solution for leet code 30 days challange day6

---
+++ leet_code/30daysofcode/day6.cpp
@@ -0,0 +1,6 @@
+// problem statement is as following:
+// Given an array of size n, find the majority element.
+// The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+
+
+// solutin function implementation is here:
