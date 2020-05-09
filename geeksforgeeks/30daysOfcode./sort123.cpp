62c6269e1099977c1675f1dce4d0f99b2233b291
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-09 14:24:44 +0530

    added solution file for geeks for geeks 30 days challange of problem sort 0 1 2

--- geeksforgeeks/30daysOfcode./sort123.cpp
+++ geeksforgeeks/30daysOfcode./sort123.cpp
@@ -3,3 +3,22 @@
 
 
 // solution function implemented here:
+
+void sort012(int a[], int n)
+{
+    int i =0, j = 0, k = n-1;
+    while(j <= k){
+        if(a[j] == 0){
+            a[j] = a[i];
+            a[i] = 0;
+            i++;
+            j++;
+        }
+        else if(a[j] == 2){
+            a[j] = a[k];
+            a[k] = 2;
+            k--;
+        }
+        else j++;
+    }
+}
