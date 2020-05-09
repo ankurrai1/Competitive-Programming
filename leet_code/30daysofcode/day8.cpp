705080507c8510dbb8dda64e02e4c445deef05cf
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-09 12:30:00 +0530

    added solution file for leetcode 30 days of challange for day 8

--- leet_code/30daysofcode/day8.cpp
+++ leet_code/30daysofcode/day8.cpp
@@ -6,3 +6,20 @@
 
 // the basic concept is slop will be same at each point and its previous points
 // slop = y2-y1 / x2-x1
+
+// solution function is as following:
+
+class Solution {
+public:
+    bool checkStraightLine(vector<vector<int>>& c) {
+        int points = c.size();
+        int xdiff = abs(c[1][0] - c[0][0]);
+        int ydiff = abs(c[1][1] - c[0][1]);
+        for(int i = 2; i < points; i++){
+            int cxdiff = abs(c[i][0] - c[i-1][0]);
+            int cydiff = abs(c[i][1] - c[i-1][1]);
+            if(xdiff * cydiff != cxdiff * ydiff ) return false;
+        }
+        return true;
+    }
+};
