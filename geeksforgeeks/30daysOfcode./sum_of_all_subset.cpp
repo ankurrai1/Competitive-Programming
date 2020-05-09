7a2c0f1add247f40496256e01f441de89a77e523
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-09 13:33:33 +0530

    added solution file for geeks for geeks 30 days challange of problem sum of all sub set of a num

--- geeksforgeeks/30daysOfcode./sum_of_all_subset.cpp
+++ geeksforgeeks/30daysOfcode./sum_of_all_subset.cpp
@@ -2,3 +2,28 @@
 // Given a number as integer s, find the sum of all the elements present in all possible subsequences of s.
 
 // solution function is implemented here:
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while(t--){
+
+        string s;
+        cin >> s;
+        int freq, sum = 0, l = s.size();
+        freq = pow(2,l-1);
+        for(int i = 0; i < l; i++){
+            sum += ((s[i] - '0') * freq);
+        }
+       cout << sum << "\n";
+    }
+    return 0;
+}
