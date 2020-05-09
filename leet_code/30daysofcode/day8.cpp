90552aa3c1fa654f471e7fc1222f06e844708f9e
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-09 12:29:39 +0530

    added solution file for leetcode 30 days of challange for day 8

--- leet_code/30daysofcode/day8.cpp
+++ leet_code/30daysofcode/day8.cpp
@@ -2,3 +2,7 @@
 
 // You are given an array coordinates, coordinates[i] = [x, y], where [x, y] represents the coordinate of a point.
 // Check if these points make a straight line in the XY plane.
+
+
+// the basic concept is slop will be same at each point and its previous points
+// slop = y2-y1 / x2-x1
