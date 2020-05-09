34a5a8fed61fce07b421495ed12240b56a5db52c
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-09 16:16:53 +0530

    added solution file for geeks for geeks 30 days challange rotate matrix

--- geeksforgeeks/30daysOfcode./rotate_matrix.cpp
+++ geeksforgeeks/30daysOfcode./rotate_matrix.cpp
@@ -10,3 +10,34 @@
 
 
 // solution function is implemented here.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--){
+        int ns;
+        cin >> ns;
+        int i = 1, ele, n = ns*ns;
+        int arr[n];
+        while(i <= ns){
+            for(int j  = 1; j <= ns; j++){
+                 cin >> ele;
+                 int it = (ns * j) - i;
+                 arr[it] = ele;
+            }
+            i++;
+        }
+        for(int i = 0; i < n; i++){
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
