8af21a1860a6bf3858e1fc90231cb440ab18f73c
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-09 14:34:24 +0530

    added solution file for geeks for geeks 30 days challange maximum in struct array

---
+++ geeksforgeeks/30daysOfcode./maximum_in_struct.cpp
@@ -0,0 +1,6 @@
+// problem statement is as following
+
+//Given a struct array of type Height, having two elements feet and inches. Find the maximum height,
+// where height is calculated sum of feet and inches after converting feet into inches.
+
+// solution function is implemented here:
