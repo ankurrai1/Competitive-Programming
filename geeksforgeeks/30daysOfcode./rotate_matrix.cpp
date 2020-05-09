b8da6a3bafcda84691994a0e4ec0f208dcda6c93
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-09 16:16:34 +0530

    added solution file for geeks for geeks 30 days challange rotate matrix

---
+++ geeksforgeeks/30daysOfcode./rotate_matrix.cpp
@@ -0,0 +1,12 @@
+// problem statement is as following:
+
+// You are given an n x n 2D matrix representing an image.
+//
+// Rotate the image by 90 degrees (clockwise).
+//
+// You need to do this in place.
+//
+// Note that if you end up using an additional array, you will only receive partial score.
+
+
+// solution function is implemented here.
