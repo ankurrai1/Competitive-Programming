829881aa78574128d120b788fb29d0b7df2d653f
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-10 13:16:31 +0530

    added solution file for leetcode 30 day may challange perfect square problem

--- leet_code/30daysofcode/validSqrt.cpp
+++ leet_code/30daysofcode/validSqrt.cpp
@@ -3,3 +3,16 @@
 //Given a positive integer num, write a function which returns True if num is a perfect square else False.
 
 // Note: Do not use any built-in library function such as sqrt.
+
+
+// solution function is as follows:
+
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        unsigned long long int i = 1;
+        while(i*i < num) i++;
+        if(i*i == num) return true;
+        return false;
+    }
+};
