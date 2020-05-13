537b7e0054c08b097fe68be0eb51f328f76d6a96
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-14 00:30:01 +0530

    added solution for geeks for geeks 30 days challange of maximum k way away array

---
+++ geeksforgeeks/30daysOfcode./nearly_sorted_array.cpp
@@ -0,0 +1,5 @@
+// problem statement is as following
+
+// Given an array of n elements,
+//  where each element is at most k away from its target position.
+//  The task is to print array in sorted form.
