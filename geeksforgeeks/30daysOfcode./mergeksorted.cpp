f51d5dc90510b8c1d28d5b369b3e0c001a40eead
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-14 19:31:47 +0530

    added file for proble mearge k length k sorted array

--- geeksforgeeks/30daysOfcode./mergeksorted.cpp
+++ geeksforgeeks/30daysOfcode./mergeksorted.cpp
@@ -5,3 +5,47 @@ Given K sorted arrays arranged in the form of a matrix. The task is to merge the
   an arr[k][k] 2D Matrix containing k sorted arrays and an integer k denoting the number of sorted arrays.
    The function should return a pointer to the merged sorted arrays.
  */
+
+// solution function is implimented here:
+
+
+// this solution is by using prerioty queue as min heap;
+
+
+typedef pair<int, int> pi;
+
+class myComparator
+{
+public:
+    int operator() (pi p1, pi p2)
+    {
+        return p1.first > p2.first;
+    }
+};
+
+// your task is tocomplete this function
+// function should return an pointer to output array int*
+// of size k*
+
+int *mergeKArrays(int arr[][N], int k)
+{
+    int n = 0, len = k * k;
+    int *ar = new int[k*k];
+
+    priority_queue<pi,vector<pi>, myComparator> pq;
+
+    for(int i = 0; i < k; i++){
+        pq.push(make_pair(arr[i][0],i));
+    }
+
+    while(!pq.empty()){
+        int rank = pq.top().second;
+
+        for(int i = 0; i < k; i++){
+            ar[n] = arr[rank][i];
+            n++;
+        }
+        pq.pop();
+    }
+    return ar;
+}
