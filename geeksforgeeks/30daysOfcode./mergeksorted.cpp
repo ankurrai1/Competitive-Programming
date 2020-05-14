a8b7778ef03191e481a5c602e725f45ef4b875fe
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-14 19:29:38 +0530

    added file for proble mearge k length k sorted array

---
+++ geeksforgeeks/30daysOfcode./mergeksorted.cpp
@@ -0,0 +1,7 @@
+// problem statement is as following :
+/*
+Given K sorted arrays arranged in the form of a matrix. The task is to merge them.
+ You need to complete mergeKArrays() function which takes 2 arguments,
+  an arr[k][k] 2D Matrix containing k sorted arrays and an integer k denoting the number of sorted arrays.
+   The function should return a pointer to the merged sorted arrays.
+ */
