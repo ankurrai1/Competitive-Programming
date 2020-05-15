4ad96a4fa0f4c7ab4c9521602894a23164a299a9
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-15 13:05:26 +0530

    added solution file for geeks for geeks 30 days challange of problem merge without extra space

--- geeksforgeeks/30daysOfcode./merge_witout_extra_space.cpp
+++ geeksforgeeks/30daysOfcode./merge_witout_extra_space.cpp
@@ -2,3 +2,41 @@
 // given two sorted arrays of size n and m;
 // your task is to merge them without using any extra space or memory
 // time complexity not more than O(n + mlog n+m);
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--){
+        int n ,m;
+        cin >> m >> n;
+        int len = m + n;
+        int arr[len];
+        for(int i = 0; i < len; i++){
+            cin >> arr[i];
+        }
+        int p = 0, p1 = m;
+        for(int i = 0; i < len; i++){
+            if(arr[p] <= arr[p1] && p < m) {
+                cout << arr[p] << " ";
+                p++;
+            }
+            else if(p1 < len){
+                cout << arr[p1] << " ";
+                p1++;
+            }
+            else{
+                cout << arr[p] << " ";
+                p++;
+            }
+        }
+
+        cout << "\n";
+    }
+	return 0;
+}
