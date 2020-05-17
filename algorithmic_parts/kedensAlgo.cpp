b9d37c06baa1dce8a53b64c33bd4ba4b8cb14f70
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-17 14:11:10 +0530

    added kadens alog basic mode

--- algorithmic_parts/kedensAlgo.cpp
+++ algorithmic_parts/kedensAlgo.cpp
@@ -4,3 +4,22 @@
 
 
 // Implement here
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n = 5;
+    int arr[n] = {1, 2, 3, -2, 5};
+    int cSum = arr[0], maxSum = arr[0];
+    for(int i = 1; i < n; i++){
+        cSum += arr[i];
+        cSum = max(cSum,arr[i]); // This is the Place where we removed previous elements or slide our window because they cant be maximum;
+        maxSum = max(cSum,maxSum);
+    }
+    cout << maxSum << "\n";
+
+    return 0;
+}
