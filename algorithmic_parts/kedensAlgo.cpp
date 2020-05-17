e9819da11eab4a82903dc31be10b50f3a6a42fec
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-17 14:10:56 +0530

    added kadens alog basic mode

---
+++ algorithmic_parts/kedensAlgo.cpp
@@ -0,0 +1,6 @@
+// Kaden's algorithm is most efficient way to solve continuous sum sub arrays problems.
+// folowing is also a variation of kaden's algorithm to solve negative numbers.  there may be many different
+// but most basic is as foolowing.
+
+
+// Implement here
