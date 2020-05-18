a4e1abd658020495a87f7cdc4f89e19ea5f89ae9
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-18 23:34:46 +0530

    added solution for geeks for geeks 30 days challange of problem nut and bolts

--- geeksforgeeks/30daysOfcode./nut_and_bolts.cpp
+++ geeksforgeeks/30daysOfcode./nut_and_bolts.cpp
@@ -6,3 +6,59 @@
 //
 // Comparison of a nut to another nut or a bolt to another bolt is not allowed.
 // It means nut can only be compared with bolt and bolt can only be compared with nut to see which one is bigger/smaller.
+
+
+// un optamized solution which didn't follow some constraints as well but passed tests.
+#include <iostream>
+using namespace std;
+#include<bits/stdc++.h>
+int main() {
+	//code
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    string s1,s2;
+	    char x;
+	    int count=0;
+	    map<int,int> m;
+	    map<int,int>:: iterator itr;
+	    for(int i=0;i<n;i++)
+	    {
+	        cin>>x;
+	        s1+=x;
+	    }
+	    for(int i=0;i<n;i++)
+	    {
+	        cin>>x;
+	        s2+=x;
+	    }
+	    for(int i=0;i<n;i++)
+	    {
+	        m[s1[i]]=i;
+	    }
+	    for(int i=0;i<n;i++)
+	    {
+	        if(m.find(s2[i])!=m.end())
+	        {
+	            count++;
+	        }
+	    }
+	    if(count==n)
+	    {
+	        for(itr =m.begin();itr!=m.end();++itr)
+	        {
+	            cout<<char(itr->first)<<" ";
+	        }
+	    }
+	    cout<<endl;
+	    for(itr =m.begin();itr!=m.end();++itr)
+	        {
+	            cout<<char(itr->first)<<" ";
+	        }
+	        cout<<endl;
+	}
+	return 0;
+}
