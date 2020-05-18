ad91a9e8dfdb56e385cb5885a4ad0d8b66d1f9e3
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-18 23:33:22 +0530

    added solution for geeks for geeks 30 days challange of problem nut and bolts

---
+++ geeksforgeeks/30daysOfcode./nut_and_bolts.cpp
@@ -0,0 +1,8 @@
+// problem statement is as following
+
+
+// Given a set of N nuts of different sizes and N bolts of different sizes.
+// There is a one-one mapping between nuts and bolts. Match nuts and bolts efficiently.
+//
+// Comparison of a nut to another nut or a bolt to another bolt is not allowed.
+// It means nut can only be compared with bolt and bolt can only be compared with nut to see which one is bigger/smaller.
