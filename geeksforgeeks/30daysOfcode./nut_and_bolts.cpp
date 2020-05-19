c6810f4c3b762504586b41868b395d67f8cc81d5
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-19 15:27:10 +0530

    added solution for geeks for geeks 30 days challange of problem nut and bolts

--- geeksforgeeks/30daysOfcode./nut_and_bolts.cpp
+++ geeksforgeeks/30daysOfcode./nut_and_bolts.cpp
@@ -8,6 +8,78 @@
 // It means nut can only be compared with bolt and bolt can only be compared with nut to see which one is bigger/smaller.
 
 
+// compared better solution solved as quick sort;
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ int partition(char arr[], int low, int high, char pivot)
+    {
+        int i = low;
+        char temp1, temp2;
+        for (int j = low; j < high; j++)
+        {
+            if (arr[j] < pivot){
+                temp1 = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp1;
+                i++;
+            } else if(arr[j] == pivot){
+                temp1 = arr[j];
+                arr[j] = arr[high];
+                arr[high] = temp1;
+                j--;
+            }
+        }
+        temp2 = arr[i];
+        arr[i] = arr[high];
+        arr[high] = temp2;
+        return i;
+    }
+
+void quick_sort_with_two(char n[], char b[], int s, int e){
+
+    if(s < e){
+        int pivot = partition(n, s, e, b[e]);
+        partition(b, s, e, n[pivot]);
+        quick_sort_with_two(n, b, s, pivot - 1);
+        quick_sort_with_two(n, b, pivot + 1, e);
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t,n;
+    cin >> t;
+    while(t--){
+        cin >> n;
+        char nuts[n];
+        char bolts[n];
+
+        for(int i = 0; i < n; i++){
+            cin >> nuts[i];
+        }
+        for(int i = 0; i < n; i++){
+            cin >> bolts[i];
+        }
+
+        quick_sort_with_two(nuts, bolts, 0, n);
+
+        for(int i = 0; i < n; i++){
+            cout << nuts[i] << " ";
+        }
+        cout << "\n";
+        for(int i = 0; i < n; i++){
+            cout << bolts[i] << " ";
+        }
+        cout << "\n";
+    }
+	return 0;
+}
+
+
 // un optamized solution which didn't follow some constraints as well but passed tests.
 #include <iostream>
 using namespace std;
