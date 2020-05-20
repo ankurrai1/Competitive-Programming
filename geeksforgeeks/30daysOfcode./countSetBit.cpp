7f2d1b78f5b5a60d7e2f8d1ec6c59eecf1c34c82
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-20 21:41:53 +0530

    added solution for geeks for geeks 30 days challange of problem count set bit using table set

--- geeksforgeeks/30daysOfcode./countSetBit.cpp
+++ geeksforgeeks/30daysOfcode./countSetBit.cpp
@@ -37,7 +37,7 @@ unsigned int countSetBits(int n)
     return count;
 }
 
-
+// built in approch in Gcc ==> cout << __builtin_popcount(4) << endl;
 
 int main() {
     int t;
@@ -48,4 +48,28 @@ int main() {
         cout << get_count(a) << "\n";
     }
 	return 0;
+}
+
+// Fastest approch to solve the
+// Mapping numbers with the bit. It simply maintains a Map(or array) of numbers to bits for a nibble.
+// num_to_bits[16] = {0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4};
+
+                   // { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };  indexes
+int num_to_bits[16] = { 0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4 }; //  count of set bits.
+
+/* Recursively get nibble of a given number
+and map them in the array */
+unsigned int countSetBitsRec(unsigned int num)
+{
+    int nibble = 0;
+    if (0 == num)
+        return num_to_bits[0];
+
+    // Find last nibble
+    nibble = num & 0xf; // it is doing & with 0000....1111 which mean we are concidered only last four bits;
+
+    // Use pre-stored values to find count
+    // in last nibble plus recursively add
+    // remaining nibbles.
+    return num_to_bits[nibble] + countSetBitsRec(num >> 4);
 }
