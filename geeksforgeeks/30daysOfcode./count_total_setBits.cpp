006b8564c6bf904dd6de0af98b995d77759e2785
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-20 21:49:05 +0530

    added solution for geeks for geeks 30 days challange of problem count total set bit

--- geeksforgeeks/30daysOfcode./count_total_setBits.cpp
+++ geeksforgeeks/30daysOfcode./count_total_setBits.cpp
@@ -1,2 +1,32 @@
 // problem statement is as following
 //You are given a number N. Find the total count of set bits for all numbers from 1 to N(both inclusive).
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int getSetBitsFromOneToN(int N){
+    int two = 2,ans = 0;
+    int n = N;
+    while(n){
+        ans += (N/two)*(two>>1);
+        if((N&(two-1)) > (two>>1)-1) ans += (N&(two-1)) - (two>>1)+1;
+        two <<= 1;
+        n >>= 1;
+    }
+    return ans;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        cout << getSetBitsFromOneToN(n) << "\n";
+    }
+	return 0;
+}
