b08da78e761b2764cbed3e726d5b727ffa9e94aa
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-22 19:13:34 +0530

    added solution for geeks for geeks 30 days challange of problem index of extra element

---
+++ geeksforgeeks/30daysOfcode./IndexOfanExtraElement.cpp
@@ -0,0 +1,5 @@
+// problem statement is as following
+// Given two sorted arrays. There is only 1 difference between the arrays.
+// First array has one element extra added in between. Find the index of the extra element.
+
+// solution is correct for above question but test cases are not testing properly;
