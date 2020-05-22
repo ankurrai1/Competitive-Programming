1191f220070486639f64bee672fe2618dd8098ab
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-22 14:53:49 +0530

    added solution for geeks for geeks 30 days challange of problem swap bits

---
+++ geeksforgeeks/30daysOfcode./swap_bits.cpp
@@ -0,0 +1,4 @@
+// problem statement is as following
+
+// Given a number x and two positions (from right side) in binary representation of x,
+// write a program that swaps n bits at given two positions and returns the result.
