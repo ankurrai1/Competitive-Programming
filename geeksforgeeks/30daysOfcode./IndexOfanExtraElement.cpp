cf08a38e0594883481788570963f83aa30b8fd90
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-22 19:13:44 +0530

    added solution for geeks for geeks 30 days challange of problem index of extra element

--- geeksforgeeks/30daysOfcode./IndexOfanExtraElement.cpp
+++ geeksforgeeks/30daysOfcode./IndexOfanExtraElement.cpp
@@ -3,3 +3,23 @@
 // First array has one element extra added in between. Find the index of the extra element.
 
 // solution is correct for above question but test cases are not testing properly;
+
+
+int findIndex(int arr[],int start,int end, int n, int ele){
+    if(start < end){
+        int mid = (start + end)/2;
+        cout << ele << " : " << arr[mid] << "\n";
+        if(arr[mid] == ele) return mid;
+        else if(arr[mid] > ele) return findIndex(arr,start,mid - 1, n , ele);
+        return findIndex(arr,mid + 1, end, n , ele);
+    }
+    return -1;
+}
+/*Complete the function below*/
+int findExtra(int a[], int b[], int n) {
+    int sumA = 0, sumB = 0;
+    for(int i = 0; i < n; i++) sumA+=a[i];
+    for(int i = 0; i < n-1; i++) sumB+=b[i];
+    int ele = sumA - sumB;
+    return (findIndex(a,0,n-1,n,ele));
+}
