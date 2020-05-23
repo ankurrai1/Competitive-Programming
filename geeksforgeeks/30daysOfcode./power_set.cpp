e3967d67dcf2f3cf46954ce31b726019e85b5338
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-23 17:39:54 +0530

    added solution for geeks for geeks 30 days challange of problem is prime string

---
+++ geeksforgeeks/30daysOfcode./power_set.cpp
@@ -0,0 +1,4 @@
+// problem statement is as following
+// Given a string of length n, print all the possible subsets of the string in a lexiographically-sorted order.
+
+// solution function is as following
