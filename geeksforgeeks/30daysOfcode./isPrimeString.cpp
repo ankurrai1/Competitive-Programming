272920b4393987f1229ff948d2f496a6fad3691f
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-23 12:52:03 +0530

    added solution for geeks for geeks 30 days challange of problem is prime string

--- geeksforgeeks/30daysOfcode./isPrimeString.cpp
+++ geeksforgeeks/30daysOfcode./isPrimeString.cpp
@@ -5,3 +5,33 @@
 
 
 // solution function is as following
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPrime(int n){
+    for(int i = 2; i*i < n; i++){
+        if(n % i == 0 ) return false;
+    }
+    return true;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--){
+        int n , x, sum = 0;
+        string s;
+        cin >> n;
+        cin >> s;
+        for(int i = 0; i < n; i++){
+            sum += (int)(s[i]);
+        }
+        if(isPrime(sum)) cout << "YES" << "\n";
+        else cout << "NO" << "\n";
+    }
+	return 0;
+}
