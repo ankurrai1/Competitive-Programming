a512643f9a553a1a6c26469af6d1b5fd80f2f14c
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-23 12:14:23 +0530

    added solution for geeks for geeks 30 days challange of problem or with multiple of x

---
+++ geeksforgeeks/30daysOfcode./ORmultipleOfX.cpp
@@ -0,0 +1,6 @@
+// problem statement is as following
+
+// Given an array A comprising of N non-zero, positive integers and an integer X,
+// find the logical OR of all such elements in the array that are a multiple of X.
+// The result of OR operation should be printed in decimal form.
+// If there is only one multiple of X in A, print the element in decimal form. If no multiple of X is found, print 0.
