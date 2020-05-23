e93e941c13afdbd1e0e405ff6cfea798b327fbf6
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-23 18:30:12 +0530

    added solution for geeks for geeks 30 days challange of problem power set

--- geeksforgeeks/30daysOfcode./power_set.cpp
+++ geeksforgeeks/30daysOfcode./power_set.cpp
@@ -6,14 +6,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+// logic behind this is to use binary like
+//                         a   b   c
+// for abc n == 3     j == 0   1   2     counter & (1 << j)
+// 000 counter == 0        0   0   0        000  & 001  = 0
+// 001 counter == 1        1   0   0        001  & 010  = 0
+// 010 counter == 2        1   0   0        010  & 100  = 0
+// 011 counter == 3
+// 100 counter == 4
+// 101 counter == 5
+// 110 counter == 6
+// 111 counter == 7
+
 void print_sub_set(string s, int n){
     vector<string> subs;
 
     unsigned int pow_set_size = pow(2, n);
     int counter;
     for(counter = 0; counter < pow_set_size; counter++){
-        string sub = "";
-        for(int j = 0; j < n; j++){
+        string sub = ""; // coutnter == 1;
+        for(int j = 0; j < n; j++){ // j will be 0 1 2 3 4 5 6 7........ n;
             if(counter & (1 << j)) sub += s[j];
         }
         subs.push_back(sub);
