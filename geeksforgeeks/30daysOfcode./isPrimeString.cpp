c221f4a334fb10cd8e976d3d97fff8276790c77c
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-23 12:51:36 +0530

    added solution for geeks for geeks 30 days challange of problem is prime string

---
+++ geeksforgeeks/30daysOfcode./isPrimeString.cpp
@@ -0,0 +1,7 @@
+// problem statement is as following
+
+//Provided a String of length N, your task is to find out whether or not the given string is a prime string.
+// A prime string is a string in which the sum of the ASCII value of all the characters is prime.
+
+
+// solution function is as following
