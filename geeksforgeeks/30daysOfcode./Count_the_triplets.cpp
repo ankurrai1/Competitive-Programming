71ec95c9b0709473168eed3ffb0afa2a132c7e3f
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-27 23:21:34 +0530

    added solution for problem of Geeks for Geeks Count the triplets

--- geeksforgeeks/30daysOfcode./Count_the_triplets.cpp
+++ geeksforgeeks/30daysOfcode./Count_the_triplets.cpp
@@ -1,4 +1,4 @@
 // problem statement is as following for problem Count the triplets.
 
-//Given an array of distinct integers.
-//The task is to count all the triplets such that sum of two elements equals the third element.
+// Given an array of distinct integers.
+// The task is to count all the triplets such that sum of two elements equals the third element.
