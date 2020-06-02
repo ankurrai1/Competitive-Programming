395209ff34b80699b9d99f90c728b010e9959bcb
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-06-02 22:35:13 +0530

    added recurcive folder and a function power

--- Random_problems/Recursion/fast_power.cpp
+++ Random_problems/Recursion/fast_power.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int power(int, int);
+int fast_power(int, int);
 
 int main(){
   ios_base::sync_with_stdio(false);
@@ -11,13 +12,20 @@ int main(){
   int a, b;
   cin >> a >> b ;
   int sum = power(a, b); // recurcive function
-  cout << sum << endl;
+  cout << sum << " " << fast_power(a, b) << endl;
   return 0;
 }
 
 int power(int a, int b){
-  if(b == 1) return a;
+  if(b == 0) return 1;
   else{
     return a * power(a , b - 1);
   }
+}
+
+int fast_power(int a, int b){
+  if(b == 0) return 1;
+  if(b % 2 == 0) return fast_power(a * a, b/2);
+  return a * power(a , b - 1);
+
 }
