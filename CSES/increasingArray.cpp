205e0bb186d8e120b87d5cdd31379f2ca4991128
Ankur Rai <rai.ankur0007@gmail.com>
2020-08-09 20:56:29 +0530

    added increasing array from CSES

--- CSES/increasingArray.cpp
+++ CSES/increasingArray.cpp
@@ -3,14 +3,18 @@
 using namespace std;
 
 int main(){
-  int n, maxdiff = 0, last, curr;
+  long long n, maxdiff = 0, last, curr;
   cin >> n;
   cin >> last;
   n--;
   while(n--){
     cin >> curr;
-    if(last - curr > 0) maxdiff += last - curr;
-    last = curr;
+    long long diff = last - curr;
+    if(diff > 0){
+        maxdiff += diff;
+        last = curr + diff;
+    }
+    else last = curr;
   }
   cout << maxdiff << "\n";
   return 0;
