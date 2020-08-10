598e61da918c3e7069afaa81c282c2b9a8a3614c
Ankur Rai <rai.ankur0007@gmail.com>
2020-08-10 20:44:17 +0530

    added count trailing 0 in factorial from CSES

--- CSES/trailingZero.cpp
+++ CSES/trailingZero.cpp
@@ -1,25 +1,14 @@
-#include <bits/stdc++.h>
 
+#include <bits/stdc++.h>
 using namespace std;
 
-int getCount(int n, int countOf){
-  int count = 0;
-  while(n % countOf == 0){
-    count++;
-    n %= countOf;
-  }
-  return count;
-}
-
 int main(){
-  long n;
-  cin >> n;
-  int count5 = 0, count2 = 0;
-  while(n){
-    if(n % 5 == 0) count5 += getCount(n, 5);
-    if(n % 2 == 0) count2 += getCount(n, 2);
-    n--;
-  }
-  cout << min(count5, count2) << "\n";
+        long n;
+        cin >> n;
+        int count = 0;
+        for(int i = 5; n / i >= 1; i *= 5){
+            count += n / 5;
+        }
+        cout << count << "\n";
   return 0;
 }
