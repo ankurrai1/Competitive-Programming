8f8a0ca53580aa0069b5d6b323e1df97316f235e
Ankur Rai <rai.ankur0007@gmail.com>
2020-08-14 23:34:27 +0530

    added codevita previous year quesitns

--- codevitaPreviousYear/countingRock.cpp
+++ codevitaPreviousYear/countingRock.cpp
@@ -7,13 +7,16 @@ int main(){
   unordered_map<int, int> range;
   cin >> n >> r;
   int arr[n];
-  int 
+  int sum[r];
   for(int i = 0; i < n; i++) cin >> arr[i];
   for(int i = 0; i < r; i++){
     int r1, r2;
     cin >> r1 >> r2;
     range[r1] = r2;
   }
-
+  int c = 0
+  for(auto e : range){
+    cout << getEleCount(arr, e.first, e.second) << " ";
+  }
   return 0;
 }
