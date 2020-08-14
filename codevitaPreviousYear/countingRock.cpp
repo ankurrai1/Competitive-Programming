b00782627e8e3d5281cfb919513e79c4875c99c6
Ankur Rai <rai.ankur0007@gmail.com>
2020-08-14 22:39:05 +0530

    added codevita previous year quesitns

--- codevitaPreviousYear/countingRock.cpp
+++ codevitaPreviousYear/countingRock.cpp
@@ -7,12 +7,13 @@ int main(){
   unordered_map<int, int> range;
   cin >> n >> r;
   int arr[n];
+  int 
   for(int i = 0; i < n; i++) cin >> arr[i];
   for(int i = 0; i < r; i++){
     int r1, r2;
     cin >> r1 >> r2;
     range[r1] = r2;
   }
-  
+
   return 0;
 }
