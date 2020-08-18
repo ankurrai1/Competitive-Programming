c09e0eaea1f2deb4e305e51ba4f07d8cd287c29b
Ankur Rai <rai.ankur0007@gmail.com>
2020-08-18 23:17:56 +0530

    added codevita previous year quesitns

--- codevitaPreviousYear/primeSum.cpp
+++ codevitaPreviousYear/primeSum.cpp
@@ -18,13 +18,11 @@ int main(){
   int arr[n];
   int sum[r];
   for(int i = 0; i < n; i++) cin >> arr[i];
-
   for(int i = 0; i < r; i++){
     int r1, r2;
     cin >> r1 >> r2;
     range[r1] = r2;
   }
-  
   int c = 0
   for(auto e : range){
     cout << getEleCount(arr, e.first, e.second) << " ";
