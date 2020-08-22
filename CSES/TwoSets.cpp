26301325d73510baf3e98d153498c059829e23f8
Ankur Rai <rai.ankur0007@gmail.com>
2020-08-23 02:32:25 +0530

    added tow set problem solution file CSES

--- CSES/TwoSets.cpp
+++ CSES/TwoSets.cpp
@@ -1,42 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define ll long long
 
-void generateSets(int n){
+void generateSets(ll n){
 
-  cout << "YES" << "\n";
+  vector<ll> set1;
 
-  vector<int> set2;
-  unordered_map<int, int> set1;
-  int tepm  = n;
+  ll half = (n * (n + 1)) / 4, sum , i, temp;
 
-  int half = (n * (n + 1)) / 4;
   if(half % n == 0){
-    int sum = n;
-    set1[n]++;
-    for (int i = 1; sum < half; i++){
-      set1[n - i]++;
-      set1[i]++;
-      sum += n;
-    }
+      set1.push_back(n);
+      sum  = temp = n;
+      i = 1;
   }
   else{
-    int sum = 0;
-    for (int i = 2; sum < half; i+=2){
-      set1[n - i]++;
-      set1[i]++;
-      sum += (n + 1);
-    }
+    sum = 0 , i = 2, temp = n +1;
   }
-  cout << set1.size() << "\n";
-  for (int i = 1; i <= n; i++){
-    if(set1[i]) cout << i << " ";
-    else set2.push_back(i);
+  while(sum < half){
+      set1.push_back(i);
+      set1.push_back(temp - i);
+      sum += (temp+1);
+      i++;
+  }
+
+  cout  << "YES" << "\n" << set1.size() << "\n";
+  sort(set1.begin(), set1.end());
+
+  string str = "";
+  ll px = 0;
+  for (ll i = 1; i <= n; i++){
+    if(set1[px] == i){
+        cout << i << " ";
+        px++;
+    }
+    else str += to_string(i) + " ";
   }
   cout << "\n";
-  cout << set2.size() << "\n";
-  for (int i = 0; i <set2.size(); i++){
-    cout <<set2[i]<< " ";
-  }
+  cout << (n - set1.size()) << "\n" << str << "\n";
+
 }
 
 
@@ -46,7 +47,7 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int n;
+    ll n;
     cin >> n;
 
     // no is odd then its not possible to half it
