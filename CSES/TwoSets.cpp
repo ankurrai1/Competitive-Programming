1d7b16b6af023db0a3fca6bd25d8733f4406d46c
Ankur Rai <rai.ankur0007@gmail.com>
2020-08-23 00:13:36 +0530

    added tow set problem solution file CSES

--- CSES/TwoSets.cpp
+++ CSES/TwoSets.cpp
@@ -7,12 +7,13 @@ void generateSets(int n){
 
   vector<int> set2;
   unordered_map<int, int> set1;
+  int tepm  = n;
 
   int half = (n * (n + 1)) / 2;
   if(half % n == 0){
     int sum = 0;
     set1[n]++;
-    for (int i = 1; sum < half;){
+    for (int i = 1; sum < half; i++){
       set[n - i]++;
       set[i]++;
       sum += n;
@@ -20,12 +21,21 @@ void generateSets(int n){
   }
   else{
     int sum = 0;
-    set1[n + 1]++;
-    for (int i = 1; sum < half;){
+    for (int i = 2; sum < half; i+=2){
       set[n - i]++;
       set[i]++;
-      sum += n;
+      sum += (n + 1);
     }
+  }
+  cout << set1.size() << "\n";
+  for (int i = 1; i <= n; i++){
+    if(set1[i]) cout << i << " ";
+    else set2.push_back(i);
+  }
+  cout << "\n";
+  cout << set2.size() << "\n";
+  for (int i = 1; i <set2.size(); i++){
+    cout <<set2[i]<< " ";
   }
 }
 
