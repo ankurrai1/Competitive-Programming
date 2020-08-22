eafd8cb1697009269faf0546ee54b8ab127dc734
Ankur Rai <rai.ankur0007@gmail.com>
2020-08-23 00:20:04 +0530

    added tow set problem solution file CSES

--- CSES/TwoSets.cpp
+++ CSES/TwoSets.cpp
@@ -9,21 +9,21 @@ void generateSets(int n){
   unordered_map<int, int> set1;
   int tepm  = n;
 
-  int half = (n * (n + 1)) / 2;
+  int half = (n * (n + 1)) / 4;
   if(half % n == 0){
-    int sum = 0;
+    int sum = n;
     set1[n]++;
     for (int i = 1; sum < half; i++){
-      set[n - i]++;
-      set[i]++;
+      set1[n - i]++;
+      set1[i]++;
       sum += n;
     }
   }
   else{
     int sum = 0;
     for (int i = 2; sum < half; i+=2){
-      set[n - i]++;
-      set[i]++;
+      set1[n - i]++;
+      set1[i]++;
       sum += (n + 1);
     }
   }
@@ -34,7 +34,7 @@ void generateSets(int n){
   }
   cout << "\n";
   cout << set2.size() << "\n";
-  for (int i = 1; i <set2.size(); i++){
+  for (int i = 0; i <set2.size(); i++){
     cout <<set2[i]<< " ";
   }
 }
@@ -46,11 +46,12 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int n, half;
+    int n;
     cin >> n;
 
     // no is odd then its not possible to half it
-    if(n & 1) cout << "NO"<<"\n";
+    int total =  (n * (n + 1)) / 2;
+    if(total & 1) cout << "NO"<<"\n";
     else generateSets(n);
 
     return 0;
