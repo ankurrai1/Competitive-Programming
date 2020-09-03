5aa091aacf850067331d0976db6a5d6b5b7da272
Ankur Rai <rai.ankur0007@gmail.com>
2020-09-03 22:30:54 +0530

    added subarray with equal 1 and 0 method in array

---
+++ MIT6006_2011/Day2/distance.cpp
@@ -0,0 +1,14 @@
+# include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    freopen("./textFiles/test1.txt", "r", stdin);
+    freopen("./textFiles/test2.txt", "r", stdin);
+
+    return 0;
+}
