19c8a0462f76b7971fa828e33459e80f9bc72269
Ankur Rai <rai.ankur0007@gmail.com>
2020-10-09 14:42:37 +0530

    added k concatination problem in leet code

---
+++ leet_code/kConcatenationMaxSum.cpp
@@ -0,0 +1,4 @@
+// Given an integer array arr and an integer k, modify the array by repeating it k times.For example, if arr = [1, 2] and k = 3 then the modified array will be [1, 2, 1, 2, 1, 2]
+
+//solution function implimented here
+
