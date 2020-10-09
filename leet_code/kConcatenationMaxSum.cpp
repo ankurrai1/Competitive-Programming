eeb40e4146139807e12d07a3cd6463f8fcc64b7d
Ankur Rai <rai.ankur0007@gmail.com>
2020-10-09 14:43:30 +0530

    added k concatination problem in leet code

--- leet_code/kConcatenationMaxSum.cpp
+++ leet_code/kConcatenationMaxSum.cpp
@@ -6,3 +6,37 @@
 
 using namespace std;
 
+#define ll long
+class Solution {
+public:
+     ll max(ll a ,ll b){
+         if(a > b) return a;
+         return b;
+     }
+    
+     ll getMaxSum(vector<int> arr, ll n){
+        int
+            mSof = arr[0],
+            mx = arr[0];
+        for(int i = 1; i<n; i++){
+            mSof = max(arr[i] , arr[i]+mSof);
+            mx = max(mx, mSof);
+        }
+        return max(mx, 0);
+    }
+    
+    ll kConcatenationMaxSum(vector<int>& arr, ll k) {
+        ll sum = 0, n = arr.size();
+        for(int i=0; i<n; i++) sum += arr[i];
+        if(k == 1){
+            return (getMaxSum(arr, n) % 1000000007);
+        }
+        vector<int> arr2(2*n);
+        for(int i = 0; i < 2*n; i++){
+            arr2[i] = arr[i % n];
+        }
+        ll mSum = getMaxSum(arr2, 2 * n);
+        if(sum > 0) mSum += (k-2)* sum;
+        return (mSum % 1000000007);
+    }
+};
