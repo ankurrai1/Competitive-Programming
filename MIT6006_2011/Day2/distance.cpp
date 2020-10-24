6b027ab8b00963a84d127a92f177eafb82f7b52a
Ankur Rai <rai.ankur0007@gmail.com>
2020-10-24 12:34:54 +0530

    reading file and parsing it complited in mit6006 day2 Document Distance

--- MIT6006_2011/Day2/distance.cpp
+++ MIT6006_2011/Day2/distance.cpp
@@ -1,5 +1,20 @@
 # include <bits/stdc++.h>
+#include <fstream>
+
 using namespace std;
+typedef map<std::string, int> FreqMap;
+
+
+void removeSpecialCharacter(string &s) 
+{ 
+    for (int i = 0; i < s.size(); i++) { 
+        s[i] = tolower(s[i]);
+        if (s[i] < 'a' || s[i] > 'z'){
+            s.erase(i, 1);  
+            i--; 
+        } 
+    } 
+} 
 
 int main(){
 
@@ -7,8 +22,30 @@ int main(){
     cin.tie(NULL);
     cout.tie(NULL);
 
-    freopen("./textFiles/test1.txt", "r", stdin);
-    freopen("./textFiles/test2.txt", "r", stdin);
+
+    ifstream file; 
+    string word, t, q, filename; 
+    FreqMap wf1, wf2;
+
+    // reading file 1;
+    filename = "./textFiles/test1.txt"; 
+    file.open(filename.c_str()); 
+    while (file >> word) 
+    { 
+        removeSpecialCharacter(word);
+        ++wf1[word];
+    } 
+    file.close();
+
+    // reading file 2
+    filename = "./textFiles/test2.txt"; 
+    file.open(filename.c_str()); 
+    while (file >> word) 
+    { 
+        removeSpecialCharacter(word);
+        ++wf2[word];
+    } 
+    file.close();
 
     return 0;
 } 
