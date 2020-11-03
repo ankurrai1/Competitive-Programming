26fbbff601dfbb4b26ff86f58ee632b7598751c5
Ankur Rai <rai.ankur0007@gmail.com>
2020-11-04 00:04:59 +0530

    added some random stuff to repo

--- Target400/Arrays/kthSmallest.cpp
+++ Target400/Arrays/kthSmallest.cpp
@@ -1,19 +1,53 @@
 #include <bits/stdc++.h>
 
-int partation(int arr[],int s, int e, int k){
-    
+using namespace std;
+
+// in this problem we are going to use quick Select algo to get result 
+
+void swap(int* a, int* b)  
+{  
+    int t = *a;  
+    *a = *b;  
+    *b = t;  
+}  
+
+void printArray(int arr[], int n){
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 }
 
-int getSmallest(int arr[], int s, int e, int k){
+int partation(int arr[],int s, int e, int k){
+    int ele = arr[e];
+    // cout << "P : " << p << endl;
+    int j = s - 1;
+    for(int i = s; i <= e - 1; i++){
+        if(arr[i] < ele){
+            j++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[j+1], &arr[e]);
+    return (j + 1);
+}
+
+void getSmallest(int arr[], int s, int e, int k, int n){
     if(s < e){
         int pos = partation(arr, s, e, k);
-        if((pos + 1) ==  k) return arr[pos];
-
+        if((pos + 1) ==  k) cout << arr[pos] << endl;
+        else if(pos + 1 > k ) return getSmallest(arr, s, pos - 1, k, n);
+        else if(pos + 1 < k) return getSmallest(arr, pos + 1, e, k, n);
     }
-    
 }
 
 int main() {
+
+    #ifndef ONLINE_JUDGE
+    freopen(".input.txt", "r", stdin) ;
+    freopen(".output.txt", "w", stdout) ;
+    #endif
+
     int t;
     cin >> t;
     while(t--){
@@ -22,7 +56,7 @@ int main() {
         int arr[n];
         for(int i = 0; i < n; i++) cin >> arr[i];
         cin >> k;
-        getSmallest(arr, 0, n - 1, k);
+        getSmallest(arr, 0, n - 1, k, n);
     }
 	return 0;
 }
