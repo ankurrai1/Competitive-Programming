7f3a2b330467dcc772c24d55c4c3e0519c447760
Ankur Rai <rai.ankur0007@gmail.com>
2020-11-14 13:46:35 +0530

    addd quick selection problem solution to target400

--- Target400/Arrays/kthSmallest.cpp
+++ Target400/Arrays/kthSmallest.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 // in this problem we are going to use quick Select algo to get result 
 
-void swap(int* a, int* b)  
-{  
-    int t = *a;  
-    *a = *b;  
-    *b = t;  
-}  
+// void swap(int* a, int* b)  
+// {  
+//     int t = *a;  
+//     *a = *b;  
+//     *b = t;  
+// }  
 
 void printArray(int arr[], int n){
     for(int i = 0; i < n; i++){
@@ -19,26 +19,29 @@ void printArray(int arr[], int n){
 }
 
 int partation(int arr[],int s, int e, int k){
+    srand(time(NULL));
+    int random = s + rand() % (e-s);
+    swap(arr[random], arr[e]);
     int ele = arr[e];
-    // cout << "P : " << p << endl;
     int j = s - 1;
     for(int i = s; i <= e - 1; i++){
         if(arr[i] < ele){
             j++;
-            swap(&arr[i], &arr[j]);
+            swap(arr[i], arr[j]);
         }
     }
-    swap(&arr[j+1], &arr[e]);
+    swap(arr[j+1], arr[e]);
     return (j + 1);
 }
 
-void getSmallest(int arr[], int s, int e, int k, int n){
+int getSmallest(int arr[], int s, int e, int k, int n){
     if(s < e){
         int pos = partation(arr, s, e, k);
-        if((pos + 1) ==  k) cout << arr[pos] << endl;
-        else if(pos + 1 > k ) return getSmallest(arr, s, pos - 1, k, n);
-        else if(pos + 1 < k) return getSmallest(arr, pos + 1, e, k, n);
+        if((pos) ==  k) return arr[pos];
+        else if(pos < n && pos > k ) return getSmallest(arr, s, pos-1, k, n);
+        else if(pos >= 0 && pos < k) return getSmallest(arr, pos+1, e, k, n);
     }
+    return arr[k];
 }
 
 int main() {
@@ -56,7 +59,7 @@ int main() {
         int arr[n];
         for(int i = 0; i < n; i++) cin >> arr[i];
         cin >> k;
-        getSmallest(arr, 0, n - 1, k, n);
+        cout << (getSmallest(arr, 0, n - 1, k - 1, n)) << endl;
     }
 	return 0;
 }
