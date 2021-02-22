6bf2ffe44221f53baaef63e9b43cc693fe5351a4
Ankur Rai <rai.ankur0007@gmail.com>
2021-02-23 04:30:30 +0530

    Problem No :      185 | leetcode reverse linked list version2 top company codes

--- leet_code/30daysofcode/reverseLL.cpp
+++ leet_code/30daysofcode/reverseLL.cpp
@@ -28,4 +28,17 @@ public:
         head->next = previousNode;
         return head;
     }
+public:
+    ListNode* reverseList2(ListNode* head) {
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        while (curr != NULL) {
+            ListNode* nextTemp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
 };
+
