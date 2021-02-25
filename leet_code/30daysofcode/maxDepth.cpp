834e09bd603d8e724801b707e6ff7504e1b5a15c
Ankur Rai <rai.ankur0007@gmail.com>
2021-02-25 20:40:10 +0530

    Problem No :      182 | leetcode find max depth of rooted tree in top company codes

---
+++ leet_code/30daysofcode/maxDepth.cpp
@@ -0,0 +1,26 @@
+// task is to find max depth of tree and
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+
+//  Definition for a binary tree node.
+struct TreeNode {
+     int val;
+      TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root == NULL)
+            return 0;
+        return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+    }
+};
