6fd150299127f5d227ae580d9a385737508082e4
Ankur Rai <rai.ankur0007@gmail.com>
2021-03-02 00:08:16 +0530

    Problem No :      188 | leetcode trapping rain water problem in top company codes

--- leet_code/30daysofcode/trappingWater.cpp
+++ leet_code/30daysofcode/trappingWater.cpp
@@ -5,6 +5,28 @@ using namespace std;
 
 
 // trapping rain water solution;
+
+// my old solution
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        if(n == 0) return 0;
+        vector<int> lmax(n,0), rmax(n,0);
+        int totalWater = 0;
+        lmax[0] = height[0]; 
+        for(int i = 1; i < n; i++) lmax[i] = max(lmax[i - 1], height[i]);
+        rmax[n-1] = height[n-1];
+        for(int i = n-2; i >= 0; i--) rmax[i] = max(rmax[i + 1], height[i]);
+        
+        for(int i = 0; i < n; i++){
+            totalWater += min(lmax[i],rmax[i]) - height[i];
+        }
+        return totalWater;
+    }
+};
+
 // better solution
 
 class Solution {
