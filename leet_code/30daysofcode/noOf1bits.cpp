3aec4e9e2ac18826c1b297c40b2d5f254468cc87
Ankur Rai <rai.ankur0007@gmail.com>
2021-03-17 20:15:09 +0530

    Problem No :      200 | leetcode count bits in given number in top Company Codes

---
+++ leet_code/30daysofcode/noOf1bits.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int count = 0; 
+        while(n){
+            count++;
+            n &= (n-1);
+        }
+        return count;
+    }
+};
