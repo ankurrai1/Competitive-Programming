e778a96086d866dde7d9ae770ee98b006143f256
Ankur Rai <rai.ankur0007@gmail.com>
2021-03-17 19:47:16 +0530

    Problem No :      198 | leetcode Pascal triangle updated in top Company Codes

--- leet_code/30daysofcode/pascalTriangle.cpp
+++ leet_code/30daysofcode/pascalTriangle.cpp
@@ -18,3 +18,20 @@ public:
         return triangle;
     }
 };
+
+
+// other version of pascal trianlge
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numofRows) {
+        vector<vector<int>> triangle(numofRows);
+        for(int i=0;i<numofRows;i++){
+            triangle[i].resize(i+1);
+            triangle[i][0] = triangle[i][i] = 1;
+            for (int j = 1; j < i; j++)
+                triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+            }
+        return triangle;
+    }
+};
