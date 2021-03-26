ed34c2c0379d97502257b601afe9b42065a156e8
Ankur Rai <rai.ankur0007@gmail.com>
2021-03-26 09:15:30 +0530

    Problem No :      270| Initial CPP codes | Pattern | added ranctangular pattern printing code

---
+++ initial_cpp_codes/pattern/printRactangularPattern.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Ternery search using recursion
+void printRectangularPattern(int row, int col){
+    for(int i = 0; i < row; i++){
+        for(int j = 0; j < col; j++){
+            cout << " * " ;
+        }
+        cout <<endl;
+    }
+}
+
+int main(){
+
+    int row = 8, col = 5;
+    printRectangularPattern(row , col);
+    return 0;
+}
