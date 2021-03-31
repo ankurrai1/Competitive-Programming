65bb2dee2ff20fe1acc86dc43c0e89e9d2d4b088
Ankur Rai <rai.ankur0007@gmail.com>
2021-03-31 08:15:30 +0530

    Problem No :      275| Initial CPP codes | Pattern | added butterFly Pattern printing code updated

--- initial_cpp_codes/pattern/butterFlyPattern.cpp
+++ initial_cpp_codes/pattern/butterFlyPattern.cpp
@@ -28,10 +28,6 @@ int main(){
 
 // less loop but time complexity will be same as above
 
-#include<bits/stdc++.h>
-using namespace std;
-
-
 void floydsTriangle(int n){
     for(int i = 1; i <= n; i++){
         for(int j = 1; j <= i; j++) cout << " * ";
