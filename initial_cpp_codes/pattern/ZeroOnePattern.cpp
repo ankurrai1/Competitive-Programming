181755f80f7623af3f6b5b3b2acf88c291175f09
Ankur Rai <rai.ankur0007@gmail.com>
2021-04-01 08:15:30 +0530

    Problem No :      277| Initial CPP codes | Pattern | added Zero One Pattern printing code Updated

--- initial_cpp_codes/pattern/ZeroOnePattern.cpp
+++ initial_cpp_codes/pattern/ZeroOnePattern.cpp
@@ -22,3 +22,22 @@ int main(){
     ZeroOnePattern(n);
     return 0;
 }
+
+// same complexity with less code
+
+void ZeroOnePattern(int n){
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= i; j++ ){
+            if((i+j) % 2 == 0) cout << " " << 1 << " ";
+            else cout << " " << 0 << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+
+    int n = 5;
+    ZeroOnePattern(n);
+    return 0;
+}
