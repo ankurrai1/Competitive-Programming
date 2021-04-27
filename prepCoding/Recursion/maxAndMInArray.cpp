d13aa69bbdc0ab5647db5ad7f37784eaa5b0eac5
Ankur Rai <rai.ankur0007@gmail.com>
2021-04-27 20:50:30 +0530

    Problem No :      236 | prepCoding | Recursion | find the max and min in the given array  Updated

--- prepCoding/Recursion/maxAndMInArray.cpp
+++ prepCoding/Recursion/maxAndMInArray.cpp
@@ -19,3 +19,25 @@ int main(){
     cout << "min : " << res.second << endl;
     return 0;
 }
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+pair<int, int> maxAndMinArray(int arr[], int n, pair<int, int> maxmin){
+    if(n == -1) return maxmin;
+    maxmin.first = max(maxmin.first, arr[n]);
+    maxmin.second = min(maxmin.second, arr[n]);
+    return maxAndMinArray(arr, n - 1, maxmin);
+ }
+
+int main(){
+    // we assumed that arry will always hava a element at least;
+    int n = 7;
+    int arr[7] = {-11,2,-3,14,5,6,17};
+    pair<int,int> res = maxAndMinArray(arr, n - 1, make_pair(arr[n-1], arr[n-1]));
+    cout << "max : " << res.first << endl;
+    cout << "min : " << res.second << endl;
+    return 0;
+}
