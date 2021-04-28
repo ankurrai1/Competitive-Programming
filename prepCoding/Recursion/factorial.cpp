5ffafc5b93e87f563c1b9af4b3b3f2ccb2992588
Ankur Rai <rai.ankur0007@gmail.com>
2021-04-28 20:50:30 +0530

    Problem No :      231 | prepCoding | Recursion | print factorial of given no

---
+++ prepCoding/Recursion/factorial.cpp
@@ -0,0 +1,16 @@
+// taks is to print factorial of given no;
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int factorial(int n){
+    if(n < 1) return 1;
+    return  n * factorial(n -1);
+}
+
+int main(){
+    int n = 6;
+    cout << factorial(n) << endl;
+    return 0;
+}
