8fce596f735988c86cf97176488a083355866892
Ankur Rai <rai.ankur0007@gmail.com>
2021-05-01 17:18:09 +0530

    Problem No :      218 | leetcode merge two sorted array in Top Companies code version 2

--- leet_code/topCompanyCodes/merge2Sorted.cpp
+++ leet_code/topCompanyCodes/merge2Sorted.cpp
@@ -1,3 +1,31 @@
+// improved solution
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i1 = m - 1;
+        int i2 = n - 1;
+        int iFinal = m + n - 1;
+        while(i1 >= 0 && i2 >= 0){
+            if(nums1[i1] >= nums2[i2]){
+                nums1[iFinal] = nums1[i1];
+                i1--;
+            }
+            else{
+                nums1[iFinal] = nums2[i2];
+                i2--;
+            }
+            iFinal--;
+        }
+        while(i2 >= 0){
+            nums1[iFinal] = nums2[i2];
+            iFinal--;
+            i2--;
+        }
+    }
+};
+
+
+// regular solution merge to sorted array
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
@@ -32,3 +60,4 @@ public:
         
     }
 };
+
