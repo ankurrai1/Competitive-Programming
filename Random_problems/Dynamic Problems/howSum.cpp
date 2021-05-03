fc3a1909a222f56b97098d6bbf60db4c662248ad
Ankur Rai <rai.ankur0007@gmail.com>
2021-05-03 20:41:10 +0530

    Problem No :      224 | Random problems | Dynamic problem | how element make target sum updated with dynamic function

--- Random_problems/Dynamic Problems/howSum.cpp
+++ Random_problems/Dynamic Problems/howSum.cpp
@@ -1,7 +1,5 @@
 #include <bits/stdc++.h>
 
-// task is to find how target sum is achived each element can be taken over and over
-
 using namespace std;
 
 void printVactor(vector<int> path){
@@ -28,6 +26,28 @@ vector<int> howSum(int target, int arr[], int n){
     return res;
 }
 
+map<int, vector<int>> mem;
+
+vector<int> fastHowSum(int target, int arr[], int n){
+    if(mem.find(target) != mem.end()) return mem[target];
+    vector<int> res;
+    if(target == 0) return res;
+    if(target < 0){
+        res.push_back(-1);
+        return res;
+    }
+    for(int i = 0; i < n; i++){
+        res = fastHowSum(target-arr[i], arr, n);
+        if(res.empty() || res[0] != -1){
+            res.push_back(arr[i]);
+            mem[target] = res;
+            return mem[target];
+        }
+    }
+    res.push_back(-1);
+    mem[target] = res;
+    return mem[target];
+}
 
 int main()
 {
@@ -41,31 +61,38 @@ int main()
     
     res = howSum(7, arr1, 2);
     if(res.empty() || res[0] != -1) printVactor(res);
-    else cout << "NULL" << endl;
+    else cout << "NULL";
     
-    cout << " -------------------"  <<endl;
+    cout << "\n-------------------"  <<endl;
     
     vector<int> res1;
     
     res1 = howSum(7, arr2, 4);
     if(res1.empty() || res1[0] != -1) printVactor(res1);
-    else cout << "NULL" << endl;
+    else cout << "NULL";
     
-    cout << " -------------------"  <<endl;
+    cout << "\n-------------------"  <<endl;
     
     vector<int> res2;
     
     res2 = howSum(7, arr3, 2);
     if(res2.empty() || res2[0] != -1) printVactor(res2);
-    else cout << "NULL" << endl;
+    else cout << "NULL";
     
-     cout << " -------------------"  <<endl;
+    cout << "\n-------------------"  <<endl;
      
     vector<int> res3;
     
     res3 = howSum(8, arr4, 3);
     if(res3.empty() || res3[0] != -1) printVactor(res3);
-    else cout << "NULL" << endl;
+    else cout << "NULL";
+    
+    cout << "\n-------------------"  <<endl;
+    
+    vector<int> res4;
+    res4 = fastHowSum(300, arr5, 2);
+    if(res4.empty() || res4[0] != -1) printVactor(res4);
+    else cout << "NULL";
     
     return 0;
 }
