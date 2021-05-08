4239874f1bc3b08a6b1cf983de9c01e598515703
Ankur Rai <rai.ankur0007@gmail.com>
2021-05-08 19:24:48 +0530

    Problem No :      241 | leetcode Pre Order binary tree traversal in Top Companies code

---
+++ leet_code/topCompanyCodes/preOrder.cpp
@@ -0,0 +1,30 @@
+// basically we have to traverse Pre order to given tree;
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+//Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    void traversal(TreeNode* root, vector<int> &res){
+        if(root == NULL) return;
+        res.push_back(root->val);
+        traversal(root->left, res);
+        traversal(root->right, res);
+    }
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> res;
+        traversal(root, res);
+        return res;
+    }
+};
