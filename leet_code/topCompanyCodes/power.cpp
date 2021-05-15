cd3d2e0abe8c532d6a78a0785cb6c5ba818f01e7
Ankur Rai <rai.ankur0007@gmail.com>
2021-05-15 23:13:30 +0530

    Problem No :      252| leetcode find negative and positive power of integers in top company codes UPDATED

--- leet_code/topCompanyCodes/power.cpp
+++ leet_code/topCompanyCodes/power.cpp
@@ -11,3 +11,23 @@ public:
         return temp;
     }
 };
+
+// iterative solution
+
+cclass Solution {
+public:
+    double myPow(double x, long long int n) {
+        if(n == 0) return 1;
+        double ans = 1;
+        if(n < 0){
+            n = -n;
+            x = 1/x;
+        }
+        while(n){
+            if(n & 1) ans *= x;
+            x *= x;
+            n>>=1;
+        }
+        return ans;
+    }
+};
