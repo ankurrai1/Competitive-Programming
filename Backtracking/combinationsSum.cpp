6a5b8c911ac431bdba27100fda6ed8968ba86e9f
Ankur Rai <rai.ankur0007@gmail.com>
2022-04-02 02:07:06 +0530

    Problem No :      308| Learning BackTracking leet code combinations sum problem my solution UPDATED

--- Backtracking/combinationsSum.cpp
+++ Backtracking/combinationsSum.cpp
@@ -34,3 +34,32 @@ public:
         return result;
     }
 };
+
+
+// my improved code solution
+class Solution {
+private:
+    void findAllCombinations(vector<vector<int>> &res,vector<int> c,int i, int t, vector<int> candi){
+        if(t == 0){
+            res.push_back(c);
+            return;
+        }
+        if(i >= candi.size() || t < 0) return;
+        for(int j = i; j < candi.size(); j++){
+            t-= candi[j];
+            c.push_back(candi[j]);
+            findAllCombinations(res, c, j, t, candi);
+            //backtrcking part
+            t += candi[j];
+            c.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> result;
+        vector<int> curr;
+        int idx = 0;
+        findAllCombinations(result, curr, idx, target, candidates);
+        return result;
+    }
+};
